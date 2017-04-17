8421a99bb80b625d208d002596e1df106ab993d8
Guo Li <guoligit@gmail.com>
2017-04-16 22:17:00 -0500

    Update 2nd question's solution

--- code2AddTwoNumbers.py
+++ code2AddTwoNumbers.py
@@ -7,4 +7,17 @@ class Solution(object):
         :type l2: ListNode
         :rtype: ListNode
         """
-        
+        carrier = 0 # carrier
+        head = node = ListNode(0) # head of the result linked list
+        while l1 or l2 or carrier:
+            num1 = num2 = 0
+            if l1 != None:
+                num1 = l1.val
+                l1 = l1.next
+            if l2 != None:
+                num2 = l2.val
+                l2 = l2.next
+            (carrier, val) = divmod(num1 + num2 + carrier, 10)
+            node.next = ListNode(val)
+            node = node.next
+        return head.next
