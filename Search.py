9ef87575854554e0ebc5abff5150ca4d39a35c3d
U-VTI\Li_Guo <Li_Guo@PIDBLVWS0352.vti.pidnet.net>
2017-06-06 16:37:44 -0500

    Add binary search python file

---
+++ Search.py
@@ -0,0 +1,17 @@
+A = [0]
+print(A, sep=' ')
+
+def BinarySearch(A, value):
+    l = 0
+    r = len(A) - 1
+    while l <= r:
+        mid = (l + r)//2
+        if A[mid]==value:
+            return mid
+        elif A[mid]<value:
+            l = mid + 1
+        else:
+            r = mid - 1
+    return -1
+
+print("The index of 0 is {}",BinarySearch(A, 0))
