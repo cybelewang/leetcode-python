faaa4c928d3916c560e134a660615f6acafdf1e7
Guo Li <guoligit@gmail.com>
2017-06-19 23:07:27 -0500

    Question 4

--- code4MedianOfTwoSortedArrays.py
+++ code4MedianOfTwoSortedArrays.py
@@ -7,10 +7,15 @@ class Solution(object):
         """
         m = len(nums1)
         n = len(nums2)
-        if m==0 and n==0:
-            return 0.0
         if m > n:
-            return findMedianSortedArrays(self, num2, nums1)
-        for i in range(m):
+            m, n, nums1, nums2 = n, m, nums2, nums1
+        if n == 0:
+            raise ValueError        
+        iMin, iMax = 0, m
+        while iMin <= iMax:
+            i = (iMin + iMax)//2
             j = (m+n+1)//2 - i
+            
+
+        
             
