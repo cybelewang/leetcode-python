9ab1cc717adbd682bc45b2ccbaa974209c907ba2
U-VTI\Li_Guo <Li_Guo@PIDBLVWS0352.vti.pidnet.net>
2017-08-04 11:04:34 -0500

    Completed problem 11 container with most water

--- code11ContainerWithMostWater.py
+++ code11ContainerWithMostWater.py
@@ -1,5 +1,6 @@
 """
-Given n non-negative integers a1, a2, ..., an, where each represents a point at coordinate (i, ai). n vertical lines are drawn such that the two endpoints of line i is at (i, ai) and (i, 0). Find two lines, which together with x-axis forms a container, such that the container contains the most water.
+Given n non-negative integers a1, a2, ..., an, where each represents a point at coordinate (i, ai). n vertical lines are drawn such that the two endpoints of line i is at (i, ai) and (i, 0). 
+Find two lines, which together with x-axis forms a container, such that the container contains the most water.
 Note: You may not slant the container and n is at least 2.
 """
 
@@ -8,3 +9,15 @@ def maxArea(self, height):
     :type height: List[int]
     :rtype: int
     """
+    i, j = 0, len(height) - 1
+    maxArea = 0
+    while i < j:
+        if height[i] < height[j]:
+            area = height[i] * (j - i)
+            i += 1
+        else:
+            area = height[j] * (j - i)
+            j -= 1
+        maxArea = max(maxArea, area)
+    
+    return maxArea
