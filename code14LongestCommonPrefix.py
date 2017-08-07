d653140aa9d9ab3a4a8bab66657526fc561c568d
Guo Li <guoligit@gmail.com>
2017-08-06 22:54:35 -0500

    Add problems 12 to 14

---
+++ code14LongestCommonPrefix.py
@@ -0,0 +1,9 @@
+"""
+Write a function to find the longest common prefix string amongst an array of strings.
+"""
+
+def longestCommonPrefix(strs):
+    """
+    :type strs: List[str]
+    :rtype: str
+    """
