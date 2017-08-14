60387431451dc519383f8c97883d5fbffe501b70
Guo Li <guoligit@gmail.com>
2017-08-13 22:06:16 -0500

    Solved problem 15 3 sum

--- code15ThreeSum.py
+++ code15ThreeSum.py
@@ -17,4 +17,32 @@ def threeSum(nums):
     :type nums: List[int]
     :rtype: List[List[int]]
     """
+    result = []
+    if len(nums) < 3:          
+        return result
+
+    nums.sort()
+
+    for i in range(len(nums)-2):
+        if (i > 0) and (nums[i] == nums[i-1]):
+            continue
+        l, r = i + 1, len(nums) - 1
+        while l < r:
+            s = nums[i] + nums[l] + nums[r]
+            if s == 0:
+                result.append([nums[i],nums[l],nums[r]])
+                l += 1
+                r -= 1                
+                while (l < r) and nums[l]==nums[l-1]: # bug fixed
+                    l += 1
+                while (l < r) and nums[r]==nums[r+1]: # bug fixed
+                    r -= 1
+            elif s < 0:
+                l += 1
+            else:
+                r -= 1      
     
+    return result
+
+test_case = [-2,0,0,2,2]
+print(threeSum(test_case))
