a5697f197b6a1c9197ab46f8564cb8e25db2c90b
Li_Guo <Li_Guo@PIDBLVWS0352.vti.pidnet.net>
2017-08-15 16:13:51 -0500

    Solved problem 18 four sum

--- code18FourSum.py
+++ code18FourSum.py
@@ -13,9 +13,39 @@ A solution set is:
 ]
 """
 
-def fourSum(self, nums, target):
+def fourSum(nums, target):
     """
     :type nums: List[int]
     :type target: int
     :rtype: List[List[int]]
     """
+    result = []
+    nums.sort()
+
+    for i in range(0,len(nums)-3):
+        if (i > 0) and (nums[i] == nums[i-1]):
+            continue
+        for j in range(i+1, len(nums)-2):
+            if (j > i + 1) and (nums[j] == nums[j-1]): # bug fixed. Don't forget condition (j > i + 1)
+                continue
+            k, l = j + 1, len(nums)-1
+            while k < l:
+                s = nums[i] + nums[j] + nums[k] + nums[l]
+                if s == target:
+                    result.append([nums[i], nums[j], nums[k], nums[l]])
+                    k += 1
+                    l -= 1
+                    while k < l and nums[k] == nums[k-1]:
+                        k += 1
+                    while k < l and nums[l] == nums[l+1]:
+                        l -= 1
+                elif s < target:
+                    k += 1
+                else:
+                    l -= 1
+    
+    return result
+
+test_case = [1, 0, -1, 0, -2, 2]
+print(fourSum(test_case, 1))
+      
