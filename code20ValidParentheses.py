1055a8aee062cf3ef145c882059da2faa516ab65
Guo Li <guoligit@gmail.com>
2017-08-14 22:19:19 -0500

    Add problem 20

---
+++ code20ValidParentheses.py
@@ -0,0 +1,13 @@
+"""
+Given a string containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+The brackets must close in the correct order, "()" and "()[]{}" are all valid but "(]" and "([)]" are not.
+
+Seen this question in a real interview before?
+"""
+
+def isValid(s):
+    """
+    :type s: str
+    :rtype: bool
+    """
