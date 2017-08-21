46627b2fc40a2039fc078f7d966842d3a1b2c471
Guo Li <guoligit@gmail.com>
2017-08-20 23:23:12 -0500

    Trial on problem 23

--- code23MergekSortedLists.py
+++ code23MergekSortedLists.py
@@ -1,8 +1,11 @@
 """
 Merge k sorted linked lists and return it as one sorted list. Analyze and describe its complexity.
 """
-def mergeKLists(self, lists):
+def mergeKLists(lists):
     """
     :type lists: List[ListNode]
     :rtype: ListNode
     """
+    lists = list(filter((lambda x: x != None), lists))
+    lists.sort(key = lambda node: node.val)
+    
