15ae0cb53488700bff4e5b24a31a7bc76fde2d9e
Li_Guo <Li_Guo@PIDBLVWS0352.vti.pidnet.net>
2017-08-23 14:09:23 -0500

    Solved problem 23 and 24

--- code23MergekSortedLists.py
+++ code23MergekSortedLists.py
@@ -6,6 +6,57 @@ Merge k sorted linked lists and return it as one sorted list. Analyze and descri
 """
 def mergeKLists(lists):
     """
+    Divide and Conquer solution
+    :type lists: List[ListNode]
+    :rtype: ListNode
+    """
+    r = len(lists) - 1
+    while r > 0:
+        # for odd number subarray, r//2 doesn't have a paired element, so decrease m by 1
+        if r % 2 == 0:
+            m = r//2 - 1
+        else:
+            m = r//2
+
+        offset = r//2 + 1 # offset between paired elements' index, which is the lenth from 0 to r//2
+        for i in range(m+1):
+            lists[i] = mergeTwoLists(lists[i], lists[i + offset])
+
+        r = r//2 # reduce the array size by a factor of 2
+
+    return lists[0]
+        
+
+def mergeTwoLists(l1, l2):
+    """
+    :type l1: ListNode
+    :type l2: ListNode
+    :rtype: ListNode
+    """
+    head = ListNode(0)
+    node = head
+    while l1 or l2:
+        if not l1:
+            node.next = l2
+            break
+        elif not l2:
+            node.next = l1
+            break
+        else:
+            if l1.val < l2.val:
+                node.next = l1
+                l1 = l1.next
+                node = node.next
+            else:
+                node.next = l2
+                l2 = l2.next
+                node = node.next
+
+    return head.next
+
+def mergeKListsMinHeap(lists):
+    """
+    Solution with MinHeap
     :type lists: List[ListNode]
     :rtype: ListNode
     """
@@ -30,5 +81,5 @@ l1, l2, l3 = ListNode(0), ListNode(0), ListNode(0)
 l1.fromList([1, 3, 5, 7, 9])
 l2.fromList([2, 4, 6, 8])
 l3.fromList([0, 2, 3, 17])
-l4 = mergeKLists([l2, l1, l3])
+l4 = mergeKLists([l1, l2, l3])
 l4.printAll()
--- code24SwapNodesInPairs.py
+++ code24SwapNodesInPairs.py
@@ -1,3 +1,5 @@
+from ListNode import ListNode
+
 """
 Given a linked list, swap every two adjacent nodes and return its head.
 
@@ -6,9 +8,44 @@ Given 1->2->3->4, you should return the list as 2->1->4->3.
 
 Your algorithm should use only constant space. You may not modify the values in the list, only nodes itself can be changed.
 """
-def swapPairs(self, head):
+def swap(n1, n2):
+    """
+    Swap two nodes and return tuple (head, tail)
+    :type n1: ListNode
+    :type n2: ListNode
+    :rtype: (ListNode, ListNode)
+    """
+    if n2:
+        tail = n2.next
+        n2.next = n1
+        n1.next = tail
+        return (n2, n1)
+    elif n1:
+        n1.next = None
+        return (n1, None)
+    else:
+        return (None, None)
+
+def swapPairs(head):
     """
     :type head: ListNode
     :rtype: ListNode
     """
-    
+    helper = ListNode(0)
+    helper.next = head
+    pre = helper
+    while head:
+        (newHead, newTail) = swap(head, head.next)
+        pre.next = newHead
+        if newTail:
+            pre = newTail
+            head = newTail.next
+        else:
+            head = None
+
+    return helper.next
+
+l1 = ListNode(0)
+l1.fromList([1, 2, 3, 4, 5])
+l2 = swapPairs(l1)
+l2.printAll()
