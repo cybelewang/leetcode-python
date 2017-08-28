00739f69243d47dbefe47a3e916ef8a5ad38f4c1
Li_Guo <Li_Guo@PIDBLVWS0352.vti.pidnet.net>
2017-08-28 16:35:41 -0500

    Solved problem 25 but bug still there

--- code25ReverseNodesInKGroup.py
+++ code25ReverseNodesInKGroup.py
@@ -15,14 +15,47 @@ For k = 2, you should return: 2->1->4->3->5
 
 For k = 3, you should return: 3->2->1->4->5
 """
-def reverseKGroup(pre, tail, k):
+def reverseKGroup2(head, tail, k):
     """
     reverse a group of k nodes with pre and tail
-    :type pre: ListNode
+    :type head: ListNode
     :type tail: ListNode
     :type k: int
     """
-    post = tail.next
+    if k < 2:
+        return (head, tail)
+    node = head
+
+    if k % 2 != 0:
+        # k is odd
+        for i in range((k-1)//2 - 1):
+            node = node.next
+        midLeft, mid, midRight = node, node.next, node.next.next
+        # keep working on left half
+        (leftHead, leftTail) = reverseKGroup2(head, midLeft, k//2)
+        # keep working on right half
+        (rightHead, rightTail) = reverseKGroup2(midRight, tail, k//2)
+        # swap left half and right half
+        rightTail.next = mid
+        mid.next = leftHead
+        # update head and tail
+        head = rightHead
+        tail = leftTail
+    else:
+        # k is even
+        for i in range((k-1)//2):
+            node = node.next
+        midLeft, midRight = node, node.next
+        # keep working on left half
+        (leftHead, leftTail) = reverseKGroup2(head, midLeft, k//2)
+        # keep working on right half
+        (rightHead, rightTail) = reverseKGroup2(midRight, tail, k//2)
+        # swap left half and right half
+        rightTail.next = leftHead
+        head = midRight
+        tail = midLeft
+
+    return (head, tail)
 
 
 def reverseKGroup(head, k):
@@ -31,4 +64,28 @@ def reverseKGroup(head, k):
     :type k: int
     :rtype: ListNode
     """
-    tail = head
+    helper = ListNode(0)
+    helper.next = head
+    pre, tail = helper, helper
+    count = 0
+    while tail:
+        tail = tail.next
+        count += 1
+        if (count == k) and tail: # bug fixed --- need to reconsider tail if it has been updated. consider the case 1->2->None with k = 3
+            nextHead = tail.next # temporarily save the next group's head
+            (newHead, newTail) = reverseKGroup2(head, tail, k) # swap this k group nodes
+            pre.next = newHead # link the pre node with the new head
+            newTail.next = nextHead # link the new tail with the previously saved next group's head
+            pre = newTail # update pre node
+            head = nextHead # update head node
+            tail = newTail # update tail node
+            count = 0
+    
+    return helper.next
+
+l1 = ListNode(0)
+l1.fromList([1, 2, 3, 4, 5, 6])
+l1.printAll()
+print()
+l2 = reverseKGroup(l1, 3)
+l2.printAll()
