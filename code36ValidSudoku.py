902847a39d503634648ed1573cf8b985972d3e71
Guo Li <guoligit@gmail.com>
2017-09-13 21:29:58 -0500

    Add problems 36-40

---
+++ code36ValidSudoku.py
@@ -0,0 +1,11 @@
+"""
+Determine if a Sudoku is valid, according to: Sudoku Puzzles - The Rules.
+
+The Sudoku board could be partially filled, where empty cells are filled with the character '.'.
+"""
+class Solution(object):
+    def isValidSudoku(self, board):
+        """
+        :type board: List[List[str]]
+        :rtype: bool
+        """
---
+++ code37SodukuSolver.py
@@ -0,0 +1,13 @@
+"""
+Write a program to solve a Sudoku puzzle by filling the empty cells.
+
+Empty cells are indicated by the character '.'.
+
+You may assume that there will be only one unique solution.
+"""
+class Solution(object):
+    def solveSudoku(self, board):
+        """
+        :type board: List[List[str]]
+        :rtype: void Do not return anything, modify board in-place instead.
+        """
