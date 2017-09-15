fde68052efb1cce2db844d47bda1a003be25e1b8
Guo Li <guoligit@gmail.com>
2017-09-14 23:31:20 -0500

    Try to solve problem 36

--- code36ValidSudoku.py
+++ code36ValidSudoku.py
@@ -1,9 +1,38 @@
 """
 Determine if a Sudoku is valid, according to: Sudoku Puzzles - The Rules.
+http://sudoku.com.au/TheRules.aspx
 
 The Sudoku board could be partially filled, where empty cells are filled with the character '.'.
 """
 class Solution(object):
+    def isRowValid(self, board):
+        record = {}        
+        for row in board: # row type: List[str]
+            record.clear()
+            for e in row:
+                if e != '.':
+                    if e not in record:
+                        record[e] = True
+                    else:
+                        return False
+                    
+        return True
+    
+    def isColumnValid(self, board):
+        record = {}
+        for i in range(len(board[0])):
+            record.clear()
+            for row in board:
+                e = row[i]
+                if e != '.':
+                    if e not in record:
+                        record[e] = True
+                    else:
+                        return False
+        
+        return True
+            
+
     def isValidSudoku(self, board):
         """
         :type board: List[List[str]]
