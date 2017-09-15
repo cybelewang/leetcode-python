e372e5e8783fbabd52e0959de4658647eb04effd
Li_Guo <Li_Guo@PIDBLVWS0352.vti.pidnet.net>
2017-09-15 16:31:14 -0500

    Solved problem 38

--- code38CountAndSay.py
+++ code38CountAndSay.py
@@ -22,10 +22,34 @@ Example 2:
 Input: 4
 Output: "1211"
 """
-class Solution(object):
-    def countAndSay(self, n):
-        """
-        :type n: int
-        :rtype: str
-        """
-        
+def countAndSay(n):
+    """
+    :type n: int
+    :rtype: str
+    """
+    if n < 1:
+        return ""
+
+    say = "1"
+
+    for i in range(n-1):
+        count = 1
+        res = ""
+
+        for j in range(1, len(say)):
+            if say[j] == say[j-1]:
+                count += 1
+            else:
+                res += str(count)
+                res += say[j-1]
+                count = 1
+        res += str(count)
+        res += say[-1]
+
+        say = res
+
+    return say    
+
+for num in range(7):
+    print(num, end=' -> ')
+    print(countAndSay(num))
