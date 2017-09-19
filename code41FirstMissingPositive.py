40418272425adede3b8741c6c5f6c58ff7d3875c
Li_Guo <Li_Guo@PIDBLVWS0352.vti.pidnet.net>
2017-09-19 16:36:33 -0500

    Fixed bugs in problem 41

--- code41FirstMissingPositive.py
+++ code41FirstMissingPositive.py
@@ -7,10 +7,35 @@ and [3,4,-1,1] return 2.
 
 Your algorithm should run in O(n) time and uses constant space.
 """
-class Solution(object):
-    def firstMissingPositive(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
-        
+
+def firstMissingPositive(nums):
+    """
+    :type nums: List[int]
+    :rtype: int
+    """
+    i, n = 0, len(nums)
+    while i < n:
+        if nums[i] < 1 or nums[i] > n or nums[i] == i + 1:
+            i += 1
+        elif nums[nums[i] - 1] != nums[i]: # bug fixed. If we don't check this, it could lead to a dead loop, for example, nums = [2, 2, 2, 2]
+            j = nums[i] - 1
+            nums[i], nums[j] = nums[j], nums[i]
+        else:
+            i += 1
+    
+    i = 0
+    # Pitfall! We should not use for loop because it may stop at i = n - 1, consider nums = [1]
+    # for i in range(n):
+    #     if nums[i] != i + 1:
+    #         break 
+   
+    while i < n and nums[i] == i + 1:
+        i += 1
+    
+    return i + 1
+
+test_cases = [[], [1], [1, 2, 0], [3, 4, -1, 1], [2, 2, 2, 2, 2]]
+
+for case in test_cases:
+    print(case, end = ' -> ')
+    print(firstMissingPositive(case))
