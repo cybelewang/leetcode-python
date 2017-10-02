9baf7f7e1a70555f0b41451b87bdb992aa005124
Guo Li <guoligit@gmail.com>
2017-10-01 20:53:19 -0500

    Added problems 51-55

---
+++ code52NQueensII.py
@@ -0,0 +1,11 @@
+"""
+Follow up for N-Queens problem.
+
+Now, instead outputting board configurations, return the total number of distinct solutions.
+"""
+class Solution(object):
+    def totalNQueens(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
