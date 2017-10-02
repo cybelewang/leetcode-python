1162aafb7462098ac6a91065c6b0aa371fc7e69f
Li_Guo <Li_Guo@PIDBLVWS0352.vti.pidnet.net>
2017-10-02 16:05:43 -0500

    Solved problem 48

--- code48RotateImage.py
+++ code48RotateImage.py
@@ -39,9 +39,29 @@ rotate the input matrix in-place such that it becomes:
   [16, 7,10,11]
 ]
 """
-class Solution(object):
-    def rotate(self, matrix):
-        """
-        :type matrix: List[List[int]]
-        :rtype: void Do not return anything, modify matrix in-place instead.
-        """
+def rotate(matrix):
+    """
+    :type matrix: List[List[int]]
+    :rtype: void Do not return anything, modify matrix in-place instead.
+    """
+    offset, N = 0, len(matrix)
+    while N > 1:
+        temp = matrix[offset][offset:offset+N-1]                
+        for i in range(N-1):
+            # left to top
+            matrix[offset][offset+i] = matrix[offset + N - 1 - i][offset]
+            # bottom to left
+            matrix[offset + N - 1 - i][offset] = matrix[offset + N - 1][offset + N - 1 - i]
+            # right to bottom
+            matrix[offset + N - 1][offset + N - 1 - i] = matrix[offset + i][offset + N - 1]
+            # top to right
+            matrix[offset + i][offset + N - 1] = temp[i]
+          
+        offset += 1
+        N -= 2
+
+test_cases = [[[]], [[1]], [[1,2],[3,4]], [[1,2,3],[4,5,6],[7,8,9]], [[5,1,9,11],[2,4,8,10],[13,3,6,7],[15,14,12,16]]]
+for A in test_cases:    
+    print(A, end=' -> ')
+    rotate(A)
+    print(A)
