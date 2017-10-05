1f523309470dc68a43d8456900cbca4279a37bc7
Li_Guo <Li_Guo@PIDBLVWS0352.vti.pidnet.net>
2017-10-05 16:17:57 -0500

    Solved problem 49

--- code49GroupAnagrams.py
+++ code49GroupAnagrams.py
@@ -11,10 +11,24 @@ Return:
 ]
 Note: All inputs will be in lower-case.
 """
-class Solution(object):
-    def groupAnagrams(self, strs):
-        """
-        :type strs: List[str]
-        :rtype: List[List[str]]
-        """
-        
+def groupAnagrams(strs):
+    """
+    :type strs: List[str]
+    :rtype: List[List[str]]
+    """
+    result = []
+    my_dict = {}
+
+    for s in strs:
+        ordered = ''.join(sorted(s))
+        if ordered in my_dict:
+            result[my_dict[ordered]].append(s)
+        else:
+            index = len(result)
+            result.append([s])
+            my_dict[ordered] = index
+    
+    return result
+
+test_case = ['a','b','ab','ba','ab']
+print(groupAnagrams(test_case))
