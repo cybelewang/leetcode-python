30946f7f583964e7fed284751e6770a68fe93b11
Li_Guo <Li_Guo@PIDBLVWS0352.vti.pidnet.net>
2017-10-09 15:58:56 -0500

    Solved problems 51-52

--- code51NQueens.py
+++ code51NQueens.py
@@ -21,51 +21,65 @@ There exist two distinct solutions to the 4-queens puzzle:
 ]
 
 """
-def _toResult(used, res):
-    sub = []
-    for row in used:
-        build = ''
-        for e in row:
-            if e:
-                build += 'Q'
-            else:
-                build += '.'
-        sub.append(build)
-    res.append(sub)
+class Solution(object):
+    def _toResult(self, used, res):
+        sub = []
+        for row in used:
+            build = ''
+            for e in row:
+                if e:
+                    build += 'Q'
+                else:
+                    build += '.'
+            sub.append(build)
+        res.append(sub)
 
-def _allowed(used, i, j):
-    # check rows
-    QueueInRow = any(used[i][:])
-    # check columns
-    QueueInCol = any(used[:][j])                
-    # check diagnol
-    
+    def _allowed(self, used, i, j, n):
+        # check rows
+        if any(used[i]):
+            return False
+        for x in range(n):
+            if used[x][j]:
+                return False
+            # check diagnol
+            y1 = x + j - i
+            y2 = j + i - x
+            if 0 <= y1 < n and used[x][y1]:
+                return False
+            if 0 <= y2 < n and used[x][y2]:
+                return False
+        
+        return True                
 
-def _NQueens(n, i, used, res):
-    # i is the row to insert a Queen
-    if i == n:
-        _toResult(used, res)
-    else:
-        bFound = False        
-        for j in range(n):
-            if _allowed(used, i, j):
-                bFound = True
-                used[i][j] = True
-                _NQueens(n, i + 1, used, res)
-                used[i][j] = False
-        if not bFound:
-            return
-    
-def solveNQueens(n):
-    """
-    :type n: int
-    :rtype: List[List[str]]
-    """
-    if n < 1:
-        return []
-    
-    used = [[False for i in range(n)] for j in range(n)]
-    res = []
-    _NQueens(n, 0, used, res)   
+    def _NQueens(self, n, i, used, res):
+        # i is the row to insert a Queen
+        if i == n:
+            self._toResult(used, res)
+        else:
+            bFound = False        
+            for j in range(n):
+                if self._allowed(used, i, j, n):
+                    bFound = True   # Found a proper place to place the Queen
+                    used[i][j] = True
+                    self._NQueens(n, i + 1, used, res)
+                    used[i][j] = False
+            if not bFound:
+                return
+        
+    def solveNQueens(self, n):
+        """
+        :type n: int
+        :rtype: List[List[str]]
+        """
+        if n < 1:
+            return []
+        
+        used = [[False for i in range(n)] for j in range(n)]
+        res = []
+        self._NQueens(n, 0, used, res)   
 
-    return res
+        return res
+
+obj = Solution()
+res = obj.solveNQueens(4)
+print(res)
--- code52NQueensII.py
+++ code52NQueensII.py
@@ -4,8 +4,50 @@ Follow up for N-Queens problem.
 Now, instead outputting board configurations, return the total number of distinct solutions.
 """
 class Solution(object):
+    t = 0
     def totalNQueens(self, n):
         """
         :type n: int
         :rtype: int
         """
+        if n < 1:
+            return 0
+
+        used = [[False for i in range(n)] for j in range(n)]
+        self._NQueens(n, 0, used)
+
+        return self.t
+    
+    def _NQueens(self, n, i, used):
+        if i == n:
+            self.t += 1
+        else:
+            bFound = False
+            for j in range(n):
+                if self._allowed(used, n, i, j):
+                    bFound = True
+                    used[i][j] = True
+                    self._NQueens(n, i + 1, used)
+                    used[i][j] = False
+            if not bFound:
+                return
+
+    def _allowed(self, used, n, i, j):
+        # check rows
+        if any(used[i]):
+            return False
+        for x in range(n):
+            if used[x][j]:
+                return False
+            # check diagnol
+            y1 = x + j - i
+            y2 = j + i - x
+            if 0 <= y1 < n and used[x][y1]:
+                return False
+            if 0 <= y2 < n and used[x][y2]:
+                return False
+        
+        return True                        
+
+obj = Solution()
+print(obj.totalNQueens(8))
