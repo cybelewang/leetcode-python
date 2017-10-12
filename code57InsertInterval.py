670d9dcecdb2f1a2b5c1eb79dfc88cc5522c3abe
Guo Li <guoligit@gmail.com>
2017-10-11 23:04:05 -0500

    Trial on problem 57

--- code57InsertInterval.py
+++ code57InsertInterval.py
@@ -11,11 +11,16 @@ Given [1,2],[3,5],[6,7],[8,10],[12,16], insert and merge [4,9] in as [1,2],[3,10
 
 This is because the new interval [4,9] overlaps with [3,5],[6,7],[8,10].
 """
-# Definition for an interval.
-# class Interval(object):
-#     def __init__(self, s=0, e=0):
-#         self.start = s
-#         self.end = e
+class Interval(object):
+    def __init__(self, s=0, e=0):
+        self.start = s
+        self.end = e
+
+    def __str__(self): # __str__ for printing single object
+        return '[' + str(self.start) + ', '+str(self.end) + ']'
+    
+    def __repr__(self): # __repr__ for printing this object in a iterable container
+        return self.__str__()
 
 class Solution(object):
     def insert(self, intervals, newInterval):
@@ -24,3 +29,15 @@ class Solution(object):
         :type newInterval: Interval
         :rtype: List[Interval]
         """
+        if not intervals:
+            return newInterval
+        elif not newInterval:
+            return intervals
+        
+        # Binary search for the insertion position
+        s, e = 0, len(intervals) - 1
+        while s <= e:
+            mid = (s + e)//2
+            if newInterval.start == intervals[mid].start:
+                 
+
