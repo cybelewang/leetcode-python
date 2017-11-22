e4d600d950c40a2d96b2de070ea89549aa5785e0
Guo Li <guoligit@gmail.com>
2017-11-21 23:38:38 -0600

    Solved problems 107-108

--- code107BinaryTreeLevelOrderTraversalII.py
+++ code107BinaryTreeLevelOrderTraversalII.py
@@ -1,3 +1,4 @@
+from TreeNode import *
 """
 Given a binary tree, return the bottom-up level order traversal of its nodes' values. (ie, from left to right, level by level from leaf to root).
 
@@ -28,4 +29,31 @@ class Solution:
         :type root: TreeNode
         :rtype: List[List[int]]
         """
+        res = []
+
+        if not root:
+            return res
+
+        level = [root]
+        while len(level) > 0:
+            newLevel = []
+            siblings = []
+            for node in level:
+                siblings.append(node.val)
+                if node.left is not None:
+                    newLevel.append(node.left)
+                if node.right is not None:
+                    newLevel.append(node.right)
+            
+            res.append(siblings)
+            level = newLevel
         
+        res.reverse()
+        
+        return res
+
+obj = Solution()
+null = None
+test_case = [3,9,20,null,null,15,7]
+test_tree = ListToTree(test_case)
+print(obj.levelOrderBottom(test_tree))
--- code108ConvertSortedArrayToBinarySearchTree.py
+++ code108ConvertSortedArrayToBinarySearchTree.py
@@ -1,3 +1,4 @@
+from TreeNode import *
 """
 Given an array where elements are sorted in ascending order, convert it to a height balanced BST.
 
@@ -15,4 +16,25 @@ class Solution:
         :type nums: List[int]
         :rtype: TreeNode
         """
+        if not nums:
+            return None
+        else:
+            return self._recursive(nums, 0, len(nums) - 1)
+
+    def _recursive(self, nums, s, e):
         
+        if s > e:
+            return None
+        elif s == e:
+            return TreeNode(nums[s])
+        else:
+            mid = (s + e)//2
+            root = TreeNode(nums[mid])
+            root.left = self._recursive(nums, s, mid - 1)
+            root.right = self._recursive(nums, mid + 1, e)
+
+            return root
+
+test_case = [1, 2, 3, 4, 5]
+obj = Solution()
+PrintTree(obj.sortedArrayToBST(test_case))
