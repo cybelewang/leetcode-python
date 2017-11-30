b06a593f28a4fc6b7841e45c5e004fa7ee86d328
Li_Guo <Li_Guo@PIDBLVWS0352.vti.pidnet.net>
2017-11-30 16:51:17 -0600

    Solved problems 119-120

--- code119PascalsTriangleII.py
+++ code119PascalsTriangleII.py
@@ -7,10 +7,29 @@ Return [1,3,3,1].
 Note:
 Could you optimize your algorithm to use only O(k) extra space?
 """
+
+# Key is to use a temp variable to save the element value before adding itself with the previous one.
 class Solution:
     def getRow(self, rowIndex):
         """
         :type rowIndex: int
         :rtype: List[int]
         """
+        if rowIndex < 0:
+            return []
+
+        build = [0 for i in range(rowIndex + 1)]    # Initialize the result array
+        build[0] = 1
+
+        for row in range(1, rowIndex + 1):
+            pre = build[0]  # this is the "original" previous value
+            for i in range(1, row + 1):
+                temp = build[i] # temporarily save the "original" value of build[i] because it will be added to the next element in the next step
+                build[i] += pre
+                pre = temp  # update the "pre" value to current, so it can be added to the next element
+            build[row] = 1  # append "1" at the end
         
+        return build
+
+obj = Solution()
+print(obj.getRow(2))
--- code120Triangle.py
+++ code120Triangle.py
@@ -14,10 +14,41 @@ Note:
 Bonus point if you are able to do this using only O(n) extra space, where n is the total number of rows in the triangle.
 
 """
+
+# Use backward adding method: curr = curr + min(upleft, upright)
 class Solution:
     def minimumTotal(self, triangle):
         """
         :type triangle: List[List[int]]
         :rtype: int
         """
+        if not triangle:
+            return 0
+
+        if len(triangle) == 1:
+            return triangle[0][0]
+
+        res = 2**31 - 1
+        for i in range(1, len(triangle)):
+            for (j, num) in enumerate(triangle[i]):
+                upLeft, upRight = j - 1, j
+                if upLeft < 0:
+                    triangle[i][j] += triangle[i-1][upRight]
+                elif upRight > i - 1:
+                    triangle[i][j] += triangle[i-1][upLeft]
+                else:
+                    triangle[i][j] += min(triangle[i-1][upLeft], triangle[i-1][upRight])
+                
+                if i == len(triangle) - 1:
+                    res = min(res, triangle[i][j])
         
+        return res
+
+obj = Solution()
+test_case = [
+     [2],
+    [3,4],
+   [6,5,7],
+  [4,1,8,3]
+]
+print(obj.minimumTotal(test_case))
