5abc0056cbee72c5af4702c3c1b37bffb6fc7a25
Guo Li <guoligit@gmail.com>
2017-12-08 00:12:03 -0600

    Added one solution for word ladder II

--- code126WordLadderII.py
+++ code126WordLadderII.py
@@ -24,6 +24,8 @@ UPDATE (2017/1/20):
 The wordList parameter had been changed to a list of strings (instead of a set of strings). Please reload the code definition to get the latest changes.
 """
 class Solution(object):
+    link = {}   # link adjacent words
+    res = []
     alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
     def findLadders(self, beginWord, endWord, wordList):
         """
@@ -32,36 +34,71 @@ class Solution(object):
         :type wordList: List[str]
         :rtype: List[List[str]]
         """
+        # convert the word list to a set
         wordSet = set(wordList)
-        if beginWord in wordSet:
-            wordSet.remove(beginWord)
+        wordSet.add(beginWord)
+        wordSet.add(endWord)
+
+        # initially set all graphic nodes' steps to MAX_INT
+        MAX_INT = 2**31 - 1
+        minSteps = MAX_INT
 
         queue = [beginWord]
-        res, build = [], [beginWord]
-        bFound = False
+        ladder = {} # cache each node's steps
 
-        while len(queue) > 0 and not bFound:
-            neighbors = []
-            for word in queue:
-                self._findNeighbors(word, neighbors, build, res, wordSet)
+        # initialize all nodes' steps to MAX_INT except the begin word
+        for word in wordSet:
+            ladder[word] = MAX_INT
+        ladder[beginWord] = 0
 
-            queue = neighbors
-            bFound = True if len(res) > 0
+        while len(queue) > 0:
+            word = queue.pop()
+            steps = ladder[word] + 1
 
-        return res
+            if steps > minSteps:    # what's this?
+                break
+            
+            for i in range(len(word)):
+                array = list(word)
+                for letter in self.alphabet:
+                    array[i] = letter
+                    new_word = ''.join(array)   # generate the new word
+                    if new_word in ladder:
+                        if steps > ladder[new_word]:
+                            continue
+                        elif steps < ladder[new_word]:
+                            queue.append(new_word)
+                            ladder[new_word] = steps
+                        
+                        if new_word in self.link:
+                            self.link[new_word].append(word)
+                        else:
+                            new_list = [word]
+                            self.link[new_word] = new_list
+                        
+                        if new_word == endWord:
+                            minSteps = steps
+
+        build = []
+        self.backTrace(endWord, beginWord, build)
+
+        return self.res
 
 
-    def _findNeighbors(self, word, neighbors, build, res, wordSet):
-        """
-        :type word: str
-        :type neighbors: list
-        :type wordSet: set
-        """      
-        for i in range(len(word)):
-            array = list(word)
-            for letter in self.alphabet:
-                array[i] = letter
-                candidate = ''.join(array)
-                if candidate in wordSet:
-                    neighbors.append(candidate)
-                    wordSet.remove(candidate)
+    def backTrace(self, word, beginWord, build):
+        if word == beginWord:
+            build.insert(0, beginWord)
+            self.res.append(build[:])
+            build.remove(beginWord)
+            return
+        build.insert(0, word)
+        if word in self.link:
+            for s in self.link[word]:
+                self.backTrace(s, beginWord, build)
+        build.remove(word)
+
+obj = Solution()
+beginWord = "hit"
+endWord = "cog"
+wordList = ["hot","dot","dog","lot","log","cog"]
+print(obj.findLadders(beginWord,endWord, wordList))
