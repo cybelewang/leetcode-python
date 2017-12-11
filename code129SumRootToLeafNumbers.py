409d9884460dc79bc67bf58db40b68cbb112097a
Li_Guo <Li_Guo@PIDBLVWS0352.vti.pidnet.net>
2017-12-11 16:23:35 -0600

    Solved problem 129

--- code129SumRootToLeafNumbers.py
+++ code129SumRootToLeafNumbers.py
@@ -1,3 +1,4 @@
+from TreeNode import *
 """
 Given a binary tree containing digits from 0-9 only, each root-to-leaf path could represent a number.
 
@@ -28,4 +29,29 @@ class Solution:
         :type root: TreeNode
         :rtype: int
         """
-        
+        if root is None:
+            return 0
+        else:
+            return self._sum(0, root)
+
+    def _sum(self, carry, root):
+        """
+        recursively calculate the sum of root-to-leaf numbers
+        carry: the carried value from upper levels
+        """
+        if root.left is None and root.right is None:
+            return carry*10 + root.val
+        elif root.left is None:
+            return self._sum(carry*10 + root.val, root.right)
+        elif root.right is None:
+            return self._sum(carry*10 + root.val, root.left)
+        else:
+            return self._sum(carry*10 + root.val, root.right) + self._sum(carry*10 + root.val, root.left)
+
+
+obj = Solution()
+null = None
+test_case = [1, 2, 5, 6, 3, null, null, 4]
+test_tree = ListToTree(test_case)
+PrintTree(test_tree)
+print(obj.sumNumbers(test_tree))
