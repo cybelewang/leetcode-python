49b65212ecded137dcaff31b8646d5083e1b76de
Li_Guo <Li_Guo@PIDBLVWS0352.vti.pidnet.net>
2017-12-13 16:36:05 -0600

    Solved problem 131, 132

--- code131PalindromePartitioning.py
+++ code131PalindromePartitioning.py
@@ -11,10 +11,46 @@ Return
   ["a","a","b"]
 ]
 """
+# DP + DFS
 class Solution:
     def partition(self, s):
         """
         :type s: str
         :rtype: List[List[str]]
         """
+        n = len(s)
+        if n < 2:
+            return [[s]]
         
+        # Get the palindrome matrix, see problem 5, longest palindromic substring
+        P = [[True for j in range(n)] for i in range(n)]
+        for i in range(n-2, -1, -1):
+            for j in range(i+1, n):
+                P[i][j] = P[i+1][j-1] and (s[i] == s[j])
+        
+        res = []
+        self._dfs(s, 0, P, [], res)
+
+        return res
+
+    def _dfs(self, s, start, P, build, res):
+        """
+        start: start index of next candidate string
+        P: the palindrome matrix
+        build: the candidate list of palindrome substrings
+        res: the result list
+        """
+        n = len(s)
+        if start == n:
+            res.append(build[:])
+            return
+
+        for end in range(start, n):
+            if P[start][end]:
+                build.append(s[start:end+1])
+                self._dfs(s, end+1, P, build, res)
+                build.pop()
+        
+obj = Solution()
+test_case = ''
+print(obj.partition(test_case))
--- code132PalindromePartitioningII.py
+++ code132PalindromePartitioningII.py
@@ -12,4 +12,35 @@ class Solution:
         :type s: str
         :rtype: int
         """
+        n = len(s)
+        if n < 2:
+            return 0
+
+        # Get the palindrome matrix, see problem 5, longest palindromic substring
+        P = [[True for j in range(n)] for i in range(n)]
+        for i in range(n-2, -1, -1):
+            for j in range(i+1, n):
+                P[i][j] = P[i+1][j-1] and (s[i] == s[j])
         
+        # BFS to find the minimum cuts
+        cuts = 0
+        queue = [0] # start index of next palindrome substring
+
+        while len(queue) > 0:
+            next_queue = []
+            for i in queue:
+                for j in range(i, n):
+                    if P[i][j]:
+                        if j == n-1:
+                            return cuts
+                        else:
+                            next_queue.append(j+1)
+            
+            queue = next_queue
+            cuts += 1
+        
+        return cuts # ideally this return statement should not be called, instead the function will return inside the while loop
+
+obj = Solution()
+test_case = 'aaaac'
+print(obj.minCut(test_case))
