230ef096265c703be831c85ccf927c10f2e29b60
Li_Guo <Li_Guo@PIDBLVWS0352.vti.pidnet.net>
2017-12-22 15:28:44 -0600

    Fixed a bug in problem 146 LRU Cache

--- code146LRUCache.py
+++ code146LRUCache.py
@@ -86,19 +86,47 @@ class DoubleLinkedList(object):
         if node is None:
             return
 
+        # save the prev and next for node
+        prev = node.prev
+        next = node.next
+
+        # disconnect node
+        node.prev = None
+        node.next = None
+
+        # process the left to right link
+        if prev is None:
+            self.head = next
+        else:
+            prev.next = next
+
+        # process the right to left link
+        if next is None:
+            self.tail = prev
+        else:
+            next.prev = prev
+
+        """ following is the original version with bugs fixed
         if self.head == node:
             # remove a head node
             self.head = node.next
+            if self.head is not None:   # bug fixed: forgot to disconnect the head with removed node
+                self.head.prev = None
             node.next = None
         elif self.tail == node:
             # remove a tail node
             self.tail = node.prev
+            if self.tail is not None:   # bug fixed: forgot to disconnect the tail with removed node
+                self.tail.next = None
             node.prev = None
         else:
             # remove a middle node
             node.prev.next = node.next
+            if node.next is not None:   # bug fixed: forgot to connect the next node's prev with previous node
+                node.next.prev = node.prev
             node.prev = None
             node.next = None
+        """
 
 class LRUCache(object):
     
@@ -123,7 +151,6 @@ class LRUCache(object):
             # move node to list head (most recently used)
             self.list.remove(node)
             self.list.appendHead(node)
-
             return node.val[1]
         else:
             return -1        
@@ -160,25 +187,27 @@ class LRUCache(object):
 # param_1 = obj.get(key)
 # obj.put(key,value)
 
-cache = LRUCache(2)
+cache = LRUCache(3)
 
-cache.put(1, 1)
-cache.put(2, 2)
-print(cache.get(1))       # returns 1
-cache.put(3, 3)    # evicts key 2
-print(cache.get(2))       # returns -1 (not found)
-cache.put(4, 4)    # evicts key 1
-print(cache.get(1))       # returns -1 (not found)
+cache.put(1, 1) # LRU: [1]
+cache.put(2, 2) # LRU: [2, 1]
+cache.put(3, 3) # LRU: [3, 2, 1]
+cache.put(4, 4)           # evicts key 1, LRU: [4, 3, 2]
+print(cache.get(4))       # returns 4, LRU: [4, 3, 2]
+print(cache.get(3))       # returns 3, LRU: [3, 4, 2]
+print(cache.get(2))       # returns 2, LRU: [2, 3, 4]
+print(cache.get(1))       # returns 2, LRU: [2, 3, 4]
+cache.put(5,5)            # evicts key 4, LRU: [5, 2, 3]
+print(cache.get(1))       # returns -1
+print(cache.get(2))       # returns 2
 print(cache.get(3))       # returns 3
-print(cache.get(4))       # returns 4
-cache.put(3,5)            # update value
-print(cache.get(4))       # returns 4
-print(cache.get(3))       # returns 5
+print(cache.get(4))       # returns -1
+print(cache.get(5))       # returns 5
 
 """
 Input:
 ["LRUCache","put","put","put","put","get","get","get","get","put","get","get","get","get","get"]
-[[3],[1,1],[2,2],[3,3],[4,4],[4],[3],[2],[1],[5,5],[1],[2],[3],[4],[5]]
+[[3],       [1,1],[2,2],[3,3],[4,4],[4],   [3],  [2],  [1], [5,5], [1],  [2],  [3],  [4],  [5]]
 Output:
 [null,null,null,null,null,4,3,2,-1,null,-1,2,-1,4,5]
 Expected:
--- code148SortList.py
+++ code148SortList.py
@@ -1,3 +1,4 @@
+from ListNode import *
 """
 Sort a linked list in O(n log n) time using constant space complexity.
 """
@@ -7,10 +8,27 @@ Sort a linked list in O(n log n) time using constant space complexity.
 #         self.val = x
 #         self.next = None
 
+# similar to merge sort, starting from sorting every two nodes, then every four node, then every 8 nodes... until 2^m >= n
 class Solution:
     def sortList(self, head):
         """
         :type head: ListNode
         :rtype: ListNode
         """
-        
+        if head is None:
+            return None
+
+        node, n = head, 0
+        while node:
+            node = node.next
+            n += 1
+
+        pre = ListNode(0)
+        pre.next = head
+
+        period = 2
+        while period < n:
+            
+            period *= 2
+
+        return pre.next
