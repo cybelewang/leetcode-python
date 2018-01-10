4853e8667cb35612db4139846520ef1284861060
Li_Guo <Li_Guo@PIDBLVWS0352.vti.pidnet.net>
2018-01-10 16:34:23 -0600

    Added two solutions for problem 189 rotate array

--- code189RotateArray.py
+++ code189RotateArray.py
@@ -11,7 +11,58 @@ Try to come up as many solutions as you can, there are at least 3 different ways
 Related problem: Reverse Words in a String II
 """
 class Solution:
+    # two pass. Reverse the whole array, then reverse the first k subarray, then reverse the remaining n-k subarray
     def rotate(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: void Do not return anything, modify nums in-place instead.
+        """
+        if not nums or k <= 0:
+            return
+        
+        n = len(nums)
+        k = k % n
+
+        # reverse the whole array
+        for i in range(n//2):
+            nums[i], nums[n-1-i] = nums[n-1-i], nums[i]
+        
+        # reverse the first k subarray
+        for i in range(k//2):
+            nums[i], nums[k-1-i] = nums[k-1-i], nums[i]
+        
+        # reverse the remaining n-k subarray
+        for i in range((n-k)//2):
+            nums[k + i], nums[n-1-i] = nums[n-1-i], nums[k + i]
+    
+    def rotate2(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: void Do not return anything, modify nums in-place instead.
+        """
+        if not nums or k <= 0:
+            return
+
+        s, n = 0, len(nums)
+
+        while n > 1 and k % n > 0: # bug fixed: if we don't have k%n > 0, it will be in dead loop for k == n
+            k = k % n
+            if k > n//2: # right part (length k) is longer, so we swap the left part (length n-k) with the most right (n-k) subarray
+                for i in range(n-k):
+                    nums[s + i], nums[s + i + k] = nums[s + i + k], nums[s + i]
+                temp = n
+                n = k # only need to process the most left k subarray because the most right n-k subarray are in good position
+                k = 2 * k - temp # we still need to rotate these steps, using a graph to analyze it
+            else: # left part (length n-k) is longer, so we swap the right part (length k) with the most left (k) subarray
+                for i in range(k):
+                    nums[s + i], nums[s + i + n - k] = nums[s + i + n - k], nums[s + i]
+                s += k # after swapping, we need to advance the start index
+                n = n - k # we still need to process the remaining (n-k) part
+
+    # use an extra memory to save the shifted out subarray, Space O(n), time O(n)
+    def rotate3(self, nums, k):
         """
         :type nums: List[int]
         :type k: int
@@ -23,14 +74,14 @@ class Solution:
         n = len(nums)
         k = k % n
 
-        remain = nums[n-k:]
-        for i in range(n-k):
+        remain = nums[n-k:] # save the shifted out subarray
+        for i in range(n-k): # shift the left part to right
             nums[n-1-i] = nums[n-k-1-i]
         
-        for i in range(k):
+        for i in range(k): # arrapy the shifted out subarray to left
             nums[i] = remain[i]
 
 test_case = [1, 2, 3, 4, 5, 6, 7]
 obj = Solution()
-obj.rotate(test_case, 3)
+obj.rotate2(test_case, 3)
 print(test_case)
