1ee8cc03a8f54a6d9f715c3b5de370f5bc031b90
Li_Guo <Li_Guo@PIDBLVWS0352.vti.pidnet.net>
2018-01-25 16:24:11 -0600

    Solved problems 213-214

--- code213HouseRobberII.py
+++ code213HouseRobberII.py
@@ -11,4 +11,32 @@ class Solution:
         :type nums: List[int]
         :rtype: int
         """
-        
+        n = len(nums)
+        # n < 3
+        if n == 0 or n == 2:
+            return 0
+        if n == 1:
+            return nums[0]
+
+        # n >= 3
+        dp = [0 for i in range(n)]
+
+        # rob from 0 to n-2
+        dp[0] = nums[0]
+        dp[1] = max(nums[0], nums[1])
+        for i in range(2, n-1):
+            dp[i] = max(dp[i-1], dp[i-2] + nums[i])
+        max0 = dp[n-2]
+
+        # rob from 1 to n-1
+        dp[0] = 0
+        dp[1] = nums[1]
+        for i in range(2, n):
+            dp[i] = max(dp[i-1], dp[i-2] + nums[i])
+        max1 = dp[n-1]
+
+        return max(max0, max1)
+
+nums = [1, 1, 1, 1, 11]
+obj = Solution()
+print(obj.rob(nums))
--- code214ShortestPalindrome.py
+++ code214ShortestPalindrome.py
@@ -13,4 +13,28 @@ class Solution:
         :type s: str
         :rtype: str
         """
+        n = len(s)
+        end = 0 # end index of the palindrome string
+        for i in range(n//2):
+            # check if even-length substring [0 to 2i+1] is palindrome
+            for j in range(i, -1, -1):
+                if s[j] != s[2*i + 1 - j]:
+                    break
+            else:
+                end = max(end, 2*i + 1)
         
+        for i in range((n+1)//2):
+            # check if odd-length substring [0 to 2i] is palindrome
+            for j in range(i-1, -1, -1):
+                if s[j] != s[2*i - j]:
+                    break
+            else:
+                end = max(end, 2*i)
+        
+        return s[end+1:][::-1] + s
+
+obj = Solution()
+test_cases = ['','a','aa','ab','aaa','aaab', 'abcd']
+for case in test_cases:
+    print(case, end=' -> ')
+    print(obj.shortestPalindrome(case))
