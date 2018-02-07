06b187307dadb0c2280f3a84b82b8645b2e4ce1c
Guo Li <guoligit@gmail.com>
2018-02-06 23:54:17 -0600

    Solved problem 218

--- code218TheSkylineProblem.py
+++ code218TheSkylineProblem.py
@@ -17,34 +17,70 @@ The input list is already sorted in ascending order by the left x position Li.
 The output list must be sorted by the x position.
 There must be no consecutive horizontal lines of equal height in the output skyline. For instance, [...[2 3], [4 5], [7 5], [11 5], [12 7]...] is not acceptable; the three lines of height 5 should be merged into one in the final output as such: [...[2 3], [4 5], [12 7], ...]
 """
+from functools import cmp_to_key
 class Solution:
     def getSkyline(self, buildings):
         """
         :type buildings: List[List[int]]
         :rtype: List[List[int]]
         """
-        encode = []   # l_h: map from Li to Hi, r_h: map from Ri to Hi
+        encode = []   # [0]: Li, [1]: Ri, [2]: 1 for rising edge, 0 for failing edge
         for b in buildings:
-            encode.append([b[0], b[1], 1])  # raising edge
+            encode.append([b[0], b[2], 1])  # raising edge
             encode.append([b[1], b[2], 0])  # falling edge
         
-        # sort the l_h by keys
-        encode = list(sorted(encode, key = lambda t: t[0]))
+        # compare function to sort the encode entries
+        def _cmp(x, y):
+            if x[0] == y[0]:
+                if x[2] == y[2]:
+                    return x[1] - y[1]
+                else:
+                    return x[2] - y[2]
+            else:
+                return x[0] - y[0]
+            
+        # sort the encode by x coordinates
+        encode = list(sorted(encode, key = cmp_to_key(_cmp)))
 
-        res, heap = [], []
+        res, heap = [], [0]
         prev_h = 0
         for b in encode:
             if b[2] == 1:
-                # raising edge
+                # rising edge
                 x, h = b[0], b[1]
                 heap.append(h)
                 heap.sort()
-                if len(res) > 0:
-                    if res[-1][0] == x:
-                                
+                if h > prev_h:
+                    res.append([x, h])
+                    prev_h = h
+            else:
+                # falling edge
+                heap.remove(b[1])
+                heap.sort()
+                h = heap[-1]
+                if h < prev_h:
+                    res.append([b[0], h])
+                    prev_h = h
+
+        # post-process res to remove the same height
+        merge = []
+        for item in res:
+            if len(merge) > 0 and merge[-1][0] == item[0]:
+                merge[-1][1] = max(merge[-1][1], item[1])
+            else:
+                merge.append(item[:])
+        
+        # remove entries with the same height
+        res = []
+        for item in merge:
+            if len(res) > 0 and res[-1][1] == item[1]:
+                pass
+            else:
+                res.append(item)
+
         return res
 
 obj = Solution()
-test_case = [[1, 2, 10], [2, 3, 10]]
-#test_case = [[2, 9, 10], [3, 7, 15], [5, 12, 12], [15, 20, 10], [19, 24, 8]]
+#test_case = [[1, 2, 10], [1, 3, 10], [3, 4, 10]]
+test_case = [[2, 9, 10], [3, 7, 15], [5, 12, 12], [15, 20, 10], [19, 24, 8]]
 print(obj.getSkyline(test_case))
