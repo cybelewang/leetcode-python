2044331596e215011e60680c649974c8c831c847
Guo Li <Guo.Li@videojet.com>
2018-03-20 16:01:35 -0500

    Fixed TLE for problem 279

--- code279PerfectSquares.py
+++ code279PerfectSquares.py
@@ -3,6 +3,7 @@ Given a positive integer n, find the least number of perfect square numbers (for
 
 For example, given n = 12, return 3 because 12 = 4 + 4 + 4; given n = 13, return 2 because 13 = 4 + 9.
 """
+# pitfall: think the largest m (m*m <= n) will get the least number
 from math import sqrt, floor
 class Solution:
     def numSquares(self, n):
@@ -13,15 +14,16 @@ class Solution:
         if n < 1:
             return 0
 
-        m = floor(sqrt(n))
-        res = 2**31-1
-        for i in range(1, m + 1):
-            res = min(res, 1 + self.numSquares(n - i**2))
+        f = [i for i in range(n+1)]
+        for i in range(4, n+1):
+            M = floor(sqrt(i))            
+            for m in range(1, M+1): # need to go through all sub squres to get the least number
+                f[i] = min(f[i], 1 + f[i - m*m])
         
-        return res
+        return f[n]
 
 
-test_cases = [1, 2, 4, 7, 12, 13, 15, 16]
+test_cases = [1, 2, 3, 4, 7, 12, 13, 15, 16]
 obj = Solution()
 for case in test_cases:
     print(case, end = '->')
--- code282ExpressionAddOperators.py
+++ code282ExpressionAddOperators.py
@@ -15,4 +15,48 @@ class Solution:
         :type target: int
         :rtype: List[str]
         """
-        
+        if len(num) < 1:
+            return []
+
+        # add operators for numbers in the list
+        # a [+/-] b [+/-/*] c, initally a = 0
+        def _add(nums, s, a, b, sign, target, build, res):
+            """
+            nums: the list of digits
+            s: start index of nums
+            a, b: numbers in operation
+            sign: 1 for '+' and -1 for '-'
+            target: the target value
+            build: the growing list
+            res: the result to be returned
+            """
+            if s == len(nums):
+                if (a + sign*b) == target:
+                    res.append(''.join(build))
+            else:
+                c = nums[s]
+                # try operator '+'
+                build.append('+')
+                build.append(str(c))
+                _add(nums, s + 1, a + sign*b, c, 1, target, build, res)
+
+                # try operator '-'
+                build[-2] = '-'
+                _add(nums, s + 1, a + sign*b, c, -1, target, build, res)
+
+                # try operator '*'
+                build[-2] = '*'
+                _add(nums, s + 1, a, b*c, sign, target, build, res)
+
+                # remove the appended strings
+                build.pop()
+                build.pop()
+
+        nums = [int(c) for c in num]
+        res = []
+        _add(nums, 1, 0, nums[0], 1, target, [num[0]], res)
+
+        return res
+
+obj = Solution()
+print(obj.addOperators('232', 8))
