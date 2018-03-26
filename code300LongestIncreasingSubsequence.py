de3df77ca7aa60920a3cbac888aac459a726312c
Guo Li <guoligit@gmail.com>
2018-03-25 23:31:51 -0500

    Add problems

--- code300LongestIncreasingSubsequence.py
+++ code300LongestIncreasingSubsequence.py
@@ -9,9 +9,89 @@ Your algorithm should run in O(n2) complexity.
 
 Follow up: Could you improve it to O(n log n) time complexity?
 """
+from collections import deque
+from bisect import *
 class Solution:
-    # brutal force
+    # dp solution, time O(nlogn)
+    # https://leetcode.com/problems/longest-increasing-subsequence/solution/
     def lengthOfLIS(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        n = len(nums)
+        if n < 1:
+            return 0
+
+        dp = [0]*n
+        maxLen = 0
+        for num in nums:
+            i = bisect_left(dp, num, 0, maxLen)
+            dp[i] = num
+            #print(dp)
+            if i == maxLen:
+                maxLen += 1
+        
+        return maxLen
+
+    # dp solution, time O(n^2)
+    # for current index i, we iterate j from 0 to i-1 (included)
+    # if nums[i] > nums[j], this means we can form a longer increasing subsequence by appending nums[i]
+    def lengthOfLIS2(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        n = len(nums)
+        if n < 1:
+            return 0
+
+        dp = [1]*n
+        for i in range(1, n):
+            maxLen = 0
+            for j in range(i):
+                if nums[i] > nums[j]:
+                    maxLen = max(maxLen, dp[j])
+            dp[i] = maxLen + 1
+        
+        return max(dp)
+
+    # TLE
+    # Graph + BFS, my own solution, time O(n^2)
+    def lengthOfLIS3(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        if len(nums) < 1:
+            return 0
+
+        # create graph
+        graph = []
+        for i in range(len(nums)):
+            connections = []    # connections contain index of the numbers > nums[i]
+            for j in range(i+1, len(nums)):
+                if nums[j] > nums[i]:
+                    connections.append(j)
+            graph.append(connections)
+        
+        queue = deque()
+        queue.extend(range(len(nums)))  # start from all possible numbers
+
+        level = 0
+        while len(queue) > 0:
+            level += 1
+            size = len(queue)
+            for i in range(size):
+                start = queue.popleft()
+                for j in graph[start]:
+                    queue.append(j)
+        
+        return level
+
+
+    # brutal force, will cause TLE
+    def lengthOfLIS4(self, nums):
         """
         :type nums: List[int]
         :rtype: int
@@ -31,4 +111,4 @@ class Solution:
 
 test_case = [10, 9, 2, 5, 3, 7, 101, 18]
 obj = Solution()
-print(obj.lengthOfLIS(test_case))
+print(obj.lengthOfLIS3(test_case))
--- code301RemoveInvalidParentheses.py
+++ code301RemoveInvalidParentheses.py
@@ -10,9 +10,112 @@ Examples:
 
 """
 class Solution:
+    # DFS
     def removeInvalidParentheses(self, s):
         """
         :type s: str
         :rtype: List[str]
         """
+        def isValid(s):
+            """
+            check if a given string s has valid parentheses
+            """
+            count = 0
+            for c in s:
+                if c == '(':
+                    count += 1
+                elif c == ')':
+                    if count == 0:
+                        return False
+                    else:
+                        count -= 1
+            
+            return count == 0
+
+        def helper(s, start, cnt1, cnt2, res):
+            """
+            recursive subfunction
+            """
+            if cnt1 == 0 and cnt2 == 0:
+                if isValid(s):
+                    res.append(s)
+                return
+            
+            for i in range(start, len(s)):
+                if i != start and s[i] == s[i-1]:   # negnect continuous same character from the second one, such as '(((' and ')))', we only process the first '(' or ')'
+                    continue
+                
+                if cnt1 > 0 and s[i] == '(':
+                    helper(s[:i] + s[i+1:], i, cnt1 - 1, cnt2, res)
+
+                if cnt2 > 0 and s[i] == ')':
+                    helper(s[:i] + s[i+1:], i, cnt1, cnt2 - 1, res)
+
+        # cnt1 : number of extra '('
+        # cnt2 : number of extra ')'
+        cnt1, cnt2 = 0, 0
+        for c in s:
+            if c == '(':
+                cnt1 += 1
+            if c == ')':
+                if cnt1 == 0:
+                    cnt2 += 1
+                else:
+                    cnt1 -= 1
         
+        res = []
+        helper(s, 0, cnt1, cnt2, res)
+
+        return res
+
+test_case = '()())()'
+obj = Solution()
+print(obj.removeInvalidParentheses(test_case))
+
+
+"""
+https://leetcode.com/problems/remove-invalid-parentheses/discuss/75027/Easy-Short-Concise-and-Fast-Java-DFS-3-ms-solution
+Key Points:
+
+Generate unique answer once and only once, do not rely on Set.
+Do not need preprocess.
+Runtime 3 ms.
+Explanation:
+We all know how to check a string of parentheses is valid using a stack. Or even simpler use a counter.
+The counter will increase when it is ‘(‘ and decrease when it is ‘)’. Whenever the counter is negative, we have more ‘)’ than ‘(‘ in the prefix.
+
+To make the prefix valid, we need to remove a ‘)’. The problem is: which one? The answer is any one in the prefix. However, if we remove any one, we will generate duplicate results, for example: s = ()), we can remove s[1] or s[2] but the result is the same (). Thus, we restrict ourself to remove the first ) in a series of concecutive )s.
+
+After the removal, the prefix is then valid. We then call the function recursively to solve the rest of the string. However, we need to keep another information: the last removal position. If we do not have this position, we will generate duplicate by removing two ‘)’ in two steps only with a different order.
+For this, we keep tracking the last removal position and only remove ‘)’ after that.
+
+Now one may ask. What about ‘(‘? What if s = ‘(()(()’ in which we need remove ‘(‘?
+The answer is: do the same from right to left.
+However a cleverer idea is: reverse the string and reuse the code!
+Here is the final implement in Java.
+
+Java
+
+public List<String> removeInvalidParentheses(String s) {
+    List<String> ans = new ArrayList<>();
+    remove(s, ans, 0, 0, new char[]{'(', ')'});
+    return ans;
+}
+
+public void remove(String s, List<String> ans, int last_i, int last_j,  char[] par) {
+    for (int stack = 0, i = last_i; i < s.length(); ++i) {
+        if (s.charAt(i) == par[0]) stack++;
+        if (s.charAt(i) == par[1]) stack--;
+        if (stack >= 0) continue;
+        for (int j = last_j; j <= i; ++j)
+            if (s.charAt(j) == par[1] && (j == last_j || s.charAt(j - 1) != par[1]))
+                remove(s.substring(0, j) + s.substring(j + 1, s.length()), ans, i, j, par);
+        return;
+    }
+    String reversed = new StringBuilder(s).reverse().toString();
+    if (par[0] == '(') // finished left to right
+        remove(reversed, ans, 0, 0, new char[]{')', '('});
+    else // finished right to left
+        ans.add(reversed);
+}
+"""
