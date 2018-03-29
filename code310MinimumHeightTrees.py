09d33d25e92b7c51228fe15a2d335cacb92501fe
Li_Guo <Li_Guo@PIDBLVWS0352.vti.pidnet.net>
2018-03-29 16:28:41 -0500

    310 minimum height trees

--- code310MinimumHeightTrees.py
+++ code310MinimumHeightTrees.py
@@ -37,10 +37,45 @@ Note:
 (2) The height of a rooted tree is the number of edges on the longest downward path between the root and a leaf.
 """
 class Solution:
+    # BFS, start from all leaves in a queue, put their connected nodes into the next level queue and disconnect them. Keep doing BFS if there are nodes with more than 1 connection
+    # Finally, the nodes with only 1 connection are what we want
     def findMinHeightTrees(self, n, edges):
         """
         :type n: int
         :type edges: List[List[int]]
         :rtype: List[int]
         """
+        if n < 1 or len(edges) < 1:
+            return []
+
+        count = [0]*n   # count[i] is the number of nodes connected to i
+        link = [[] for i in range(n)]   # link[i] stores the nodes that connected to i
+        for (first, second) in edges:
+            link[first].append(second)
+            count[first] += 1
+            link[second].append(first)
+            count[second] += 1
         
+        queue = set()
+        for i in range(n):
+            if count[i] == 1:
+                queue.add(i)
+
+        # BFS        
+        while max(count) > 1:
+            next_queue = set()
+            for first in queue:
+                second = link[first][0]
+                next_queue.add(second)
+                link[first].remove(second)
+                count[first] -= 1
+                link[second].remove(first)
+                count[second] -= 1
+        
+            queue = next_queue
+        
+        return list(queue)
+
+obj = Solution()
+edges = [[0,3], [1,3], [2,3], [3,4], [4,5], [5,7],[5,8], [5,6]]
+print(obj.findMinHeightTrees(9, edges))
