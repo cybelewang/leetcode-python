66997ea2a612a7d66c2aa9c5fa5a27c0751a8c8f
Li_Guo <Li_Guo@PIDBLVWS0352.vti.pidnet.net>
2018-04-10 16:18:05 -0500

    341

--- code341FlattenNestedListIterator.py
+++ code341FlattenNestedListIterator.py
@@ -38,25 +38,80 @@ By calling next repeatedly until hasNext returns false, the order of elements re
 #        :rtype List[NestedInteger]
 #        """
 
+# queue solution
+from collections import deque
 class NestedIterator(object):
+    
+    def __init__(self, nestedList):
+        """
+        Initialize your data structure here.
+        :type nestedList: List[NestedInteger]
+        """
+        self.queue = deque()
+        for ni in nestedList:
+            self.queue.append(ni)
+
+    def next(self):
+        """
+        :rtype: int
+        """
+        ni = self.queue.popleft()
+        return ni.getInteger()
+
+    def hasNext(self):
+        """
+        :rtype: bool
+        """
+        while len(self.queue) > 0:
+            if self.queue[0].isInteger():
+                return True
+            else:
+                nis = self.queue.popleft()
+                self.queue.extendleft(nis.getList[::-1])
+        
+        return False
+
+# recursive solution
+class NestedIterator2(object):
+
+    def extract(self, nestedInteger):
+        """
+        extract integer from input NestedInteger
+        :type nestedInteger: NestedInteger
+        :rtype list of integer
+        """
+        if nestedInteger.isInteger():
+            return [nestedInteger.getInteger()]
+        
+        res = []
+        for item in nestedInteger.getList():
+            res.extend(self.extract(item))
+
+        return res            
 
     def __init__(self, nestedList):
         """
         Initialize your data structure here.
         :type nestedList: List[NestedInteger]
         """
-        
+        self.data = []
+        for item in nestedList:
+            self.data.extend(self.extract(item))
+
+        self.index = 0
 
     def next(self):
         """
         :rtype: int
         """
-        
+        return self.data[self.index]
+        self.index += 1
 
     def hasNext(self):
         """
         :rtype: bool
         """
+        return self.index < len(self.data)
         
 
 # Your NestedIterator object will be instantiated and called as such:
