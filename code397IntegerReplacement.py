1b604f0387fe952e624c0ecd972fa9ba556aad26
Li_Guo <Li_Guo@PIDBLVWS0352.vti.pidnet.net>
2018-05-02 16:38:26 -0500

    397, 398

--- code397IntegerReplacement.py
+++ code397IntegerReplacement.py
@@ -29,9 +29,61 @@ or
 7 -> 6 -> 3 -> 2 -> 1
 """
 class Solution:
+    # http://bookshadow.com/weblog/2016/09/11/leetcode-integer-replacement/
+    # if n is even, n //= 2
+    # if n is odd, check the lowest 2 bits: for '01', n -= 1 to acheive '00'; for '11', n += 1 to achieve '00', the purpose is to reduce number of '1' in n
+    # 3 is an exception
     def integerReplacement(self, n):
         """
         :type n: int
         :rtype: int
         """
+        res = 0
+        while n != 1:
+            if n & 1 == 0:
+                n //= 2
+            elif n == 3 or ((n//2) & 1 == 0):
+                n -= 1
+            else:
+                n += 1
+            res += 1
         
+        return res
+
+    # recursive solution, since the depth is about log2(n), not very large
+    def integerReplacement2(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        if n == 1: return 0
+        if n & 1 == 0: return self.integerReplacement2(n//2) + 1
+        return min(self.integerReplacement2(n+1), self.integerReplacement2(n-1)) + 1
+
+    # my own DP solution, overflow error for very large n, such as 2**31-1
+    def integerReplacement3(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        if n == 1:
+            return 0
+
+        dp = [0]*(n+1)
+        dp[1] = 0
+
+        for i in range(2, n+1):
+            if i % 2 == 0:
+                # i is even
+                dp[i] = dp[i//2] + 1
+            else:
+                # i is odd
+                dp[i] = min(dp[(i-1)//2], dp[(i+1)//2]) + 2
+        
+        return dp[n]
+
+obj = Solution()
+test_cases = [1, 2, 3, 4, 7, 8, 100, 2**31-1]
+for case in test_cases:
+    print(case, end=' -> ')
+    print(obj.integerReplacement2(case))
--- code398RandomPickIndex.py
+++ code398RandomPickIndex.py
@@ -15,22 +15,35 @@ solution.pick(3);
 // pick(1) should return 0. Since in the array only nums[0] is equal to 1.
 solution.pick(1);
 """
+# reference http://www.cnblogs.com/grandyang/p/5875509.html
+from random import randrange
 class Solution:
 
     def __init__(self, nums):
         """
         :type nums: List[int]
         """
-        
+        self.data = nums        
 
     def pick(self, target):
         """
         :type target: int
         :rtype: int
         """
-        
+        res, count = -1, 0
+        for i, num in enumerate(self.data):
+            if num == target:
+                count += 1
+                if res == -1 or randrange(0, count) == 0:   # here res == -1 is redundant because the first time encounts a target, count == 1 and randrange(0, 1) must return 0
+                    res = i
+       
+        return res
 
-
+nums = [1,2,3,3,3]
+obj = Solution(nums)
+print(obj.pick(3))
+print(obj.pick(3))
+print(obj.pick(3))
 # Your Solution object will be instantiated and called as such:
 # obj = Solution(nums)
 # param_1 = obj.pick(target)
