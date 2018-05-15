18c4ad63f06da64a0becdd9e031df0d84193b470
Li_Guo <Li_Guo@PIDBLVWS0352.vti.pidnet.net>
2018-05-15 16:47:50 -0500

    421, 423

--- code421MaximumXORofTwoNumbersInAnArray.py
+++ code421MaximumXORofTwoNumbersInAnArray.py
@@ -1,5 +1,5 @@
 """
-Given a non-empty array of numbers, a0, a1, a2, … , an-1, where 0 ≤ ai < 231.
+Given a non-empty array of numbers, a0, a1, a2, … , an-1, where 0 ≤ ai < 2**31.
 
 Find the maximum result of ai XOR aj, where 0 ≤ i, j < n.
 
@@ -9,14 +9,44 @@ Example:
 
 Input: [3, 10, 5, 25, 2, 8]
 
+3   00011
+10  01010
+5   00101
+25  11001
+2   00010
+8   01000
+
 Output: 28
 
 Explanation: The maximum result is 5 ^ 25 = 28.
 """
 class Solution:
+    # Let's figure out how to get the high nth bit in result
+    # for each number, keep only the highest n bits, set (32-n) lowest bits to 0, and put the result into a set
+    # create a number tmp equal to result, then set the nth bit of tmp to be 1
+    # try to find two pre-n-bit numbers in the set that their XOR result is tmp. This can be done through checking if tmp ^ pre in set, because if pre1 ^ pre2 = tmp, then pre1 ^ tmp = pre2. This is similar to "Two Sum", using space to exchange with time
+    # If we cannot find the above two pre-n-bit numbers in the set, that means nth bit should be 0 in result
     def findMaximumXOR(self, nums):
         """
         :type nums: List[int]
         :rtype: int
         """
-        
+        # if a ^ b = x, then a ^ x = b, a = b ^ x
+        # https://kingsfish.github.io/2017/12/15/Leetcode-421-Maximum-XOR-of-Two-Numbers-in-an-Array/
+        res, mask = 0, 0
+        for i in range(31, -1, -1):
+            mask = mask | (1 << i)  # n = 32-i, all n high bits are '1'
+            myset = set()
+            for num in nums:
+                myset.add(mask & num) # put the prefix n high bits of each number into myset
+            
+            tmp = res | (1 << i)    # assume nth bit in result is '1', this makes sure the result is the largest
+            for pre in myset:
+                if (tmp ^ pre) in myset: # test if pre ^ (another pre in mytest) == tmp
+                    res = tmp   # there are two pre in mytest and their XOR is tmp, so update res to tmp
+                    break
+            
+        return res
+
+nums = [3, 10, 5, 25, 2, 8]
+print(Solution().findMaximumXOR(nums))
--- code423ReconstructOriginalDigitsFromEnglish.py
+++ code423ReconstructOriginalDigitsFromEnglish.py
@@ -13,6 +13,23 @@ Example 2:
 Input: "fviefuro"
 
 Output: "45"
+
+first round to find the fingerprint letter
+zero    ->      eorz    ->  z(1)
+two     ->      otw     ->  w(1)
+four    ->      foru    ->  u(1)
+six     ->      isx     ->  x(1)
+
+second round to find the fingerprint letter
+one     ->      eno     ->  o(2)
+three   ->      ehrt    ->  r(2)
+five    ->      efiv    ->  f(2)
+seven   ->      ensv    ->  s(2)
+eight   ->      eghit   ->  g(2)
+
+third round to find the fingerprint letter
+nine    ->      ein     ->  e(3)
+
 """
 class Solution:
     def originalDigits(self, s):
@@ -20,4 +37,19 @@ class Solution:
         :type s: str
         :rtype: str
         """
+        digit_count = [0]*10
+        letter_count = [0]*26
+        base = ord('a')
         
+        fp = [('z', 'zero', 0), ('w', 'two', 2), ('u', 'four', 4), ('x', 'six', 6), ('o', 'one', 1), ('r', 'three', 3), ('f', 'five', 5), ('s', 'seven', 7), ('g', 'eight', 8), ('e', 'nine', 9)]
+        
+        for c in s:
+            letter_count[ord(c) - base] += 1
+        
+        for key, s, num in fp:
+            digit_count[num] = letter_count[ord(key) - base]
+            for c in s:
+                letter_count[ord(c) - base] -= digit_count[num]
+
+        return ''.join(map(str, sum([i]*digit_count[i] for i in range(10), [])))        
+
