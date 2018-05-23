4c9feea20e60e79381a2e4065aca67106d2ed939
Guo Li <guoligit@gmail.com>
2018-05-22 23:47:19 -0500

    438

--- code438FindAllAnagramsInAString.py
+++ code438FindAllAnagramsInAString.py
@@ -31,10 +31,48 @@ The substring with start index = 1 is "ba", which is an anagram of "ab".
 The substring with start index = 2 is "ab", which is an anagram of "ab".
 """
 class Solution:
+    # use 26 bits to represent if each s's letter's count is the same as p
     def findAnagrams(self, s, p):
         """
         :type s: str
         :type p: str
         :rtype: List[int]
         """
+        count_s, count_p, base = [0]*26, [0]*26, ord('a')
+        expected = 0
+        for c in p:
+            index = ord(c) - base
+            count_p[index] += 1
+            expected |= 1 << index  # bug fixed: initially set all 26 bits to 1, we should leave the unused letter's bit as 0
         
+        i, encode = 0, 0
+        res = []
+        for j, c in enumerate(s):
+            index = ord(c) - base
+            count_s[index] += 1
+            if count_s[index] == count_p[index]:
+                encode |= 1<<index
+            else:
+                encode &= expected - (1<<index)
+            #if encode == expected:
+            #    res.append(i)
+            if j - i + 1 == len(p):
+                if encode == expected:
+                    res.append(i)
+                    
+                index = ord(s[i]) - base
+                count_s[index] -= 1
+                # bug fixed: forgot to update the encode
+                if count_s[index] == count_p[index]:
+                    encode |= 1<<index
+                else:
+                    encode &= expected - (1<<index)
+
+                i += 1
+        
+        return res
+
+s= "baa"
+p= "aa"
+obj = Solution()
+print(obj.findAnagrams(s, p))
