98a84336ea9e6143181782f2a9ccf8e6fd9ff1b5
Guo Li <guoligit@gmail.com>
2018-05-23 23:39:51 -0500

    441

--- code441ArrangingCoins.py
+++ code441ArrangingCoins.py
@@ -27,10 +27,21 @@ The coins can form the following rows:
 
 Because the 4th row is incomplete, we return 3.
 """
+# (i+2)*(i+1) > 2*n
+# i*(i+1) <= 2*n
+from math import ceil
 class Solution:
     def arrangeCoins(self, n):
         """
         :type n: int
         :rtype: int
         """
-        
+        n *= 2
+        i = ceil(n**0.5)    # initialize i to the biggest possible value
+        while i*(i+1) > n:  # decrease i until it meets the conditions
+            i -= 1
+
+        return i
+
+obj = Solution()
+print(obj.arrangeCoins(2**31-1))
