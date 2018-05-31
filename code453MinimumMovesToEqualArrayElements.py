92ef16cd44460258f4fcd2f00aeebc6ef11d7fd7
Guo Li <guoligit@gmail.com>
2018-05-30 23:31:22 -0500

    453

--- code453MinimumMovesToEqualArrayElements.py
+++ code453MinimumMovesToEqualArrayElements.py
@@ -15,9 +15,18 @@ Only three moves are needed (remember each move increments two elements):
 [1,2,3]  =>  [2,3,3]  =>  [3,4,3]  =>  [4,4,4]
 """
 class Solution:
+    # my own solution!
+    # the min of nums will always be added 1
+    # assume the final equal value is M, then M - min is what we want, and we assume x = M - min
+    # The sum of the final equal list is n*M, where n is the length of array
+    # We know that n*M = sum(nums) + (n-1)*x = n(x+min), so x = sum(nums) - n*min
     def minMoves(self, nums):
         """
         :type nums: List[int]
         :rtype: int
         """
-        
+        return sum(nums) - len(nums)*min(nums)
+
+nums = [1, 2, 3]
+obj = Solution()
+print(obj.minMoves(nums))
