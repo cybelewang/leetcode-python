455fe543fe1934c988381d9fce2edb8096bbf9f0
Guo Li <guoligit@gmail.com>
2018-06-04 00:11:26 -0500

    457, added 461-462

--- code457CircularArrayLoop.py
+++ code457CircularArrayLoop.py
@@ -14,10 +14,56 @@ Note: The given array is guaranteed to contain no element "0".
 Can you do it in O(n) time complexity and O(1) space complexity?
 
 """
+# understand this problem correctly
+# 1. The loop must be "forward" or "backward'. This means it must loop on one direction. FOr example, [1, -1] is not valid
+# 2. A loop starts and ends at a particular index with more than 1 element along the loop. For example, [-1, 2], where 2 goes to 2, and the start index = end index. This is invalid.
+# 3. the initial position is not necessarily the zeroth element. [3, 1, 2] is a loop because 1->2->1
 class Solution(object):
+    # OJ sample solution, O(1) space
     def circularArrayLoop(self, nums):
         """
         :type nums: List[int]
         :rtype: bool
         """
+        for i in range(len(nums)):
+            start = i
+            if nums[i] == 0:
+                continue
+            positive = nums[start] > 0
+            if nums[start] % len(nums) == 0:
+                continue
+            i_next = (i+nums[start]) % len(nums)
+            while nums[i_next] % len(nums) != 0:
+                if positive != (nums[i_next] > 0):
+                    break
+                if i_next == start:
+                    return True
+                tmp = nums[i_next]
+                nums[i_next] = 0
+                i_next = (i_next+tmp)%len(nums)
+        return False
+    # https://www.cnblogs.com/grandyang/p/7658128.html
+    def circularArrayLoop2(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        n = len(nums)
+        mem, visited = {}, [False]*n
+        for i in range(n):
+            if visited[i]: continue
+            cur = i
+            while True:
+                visited[cur] = True
+                next = (cur + nums[cur] + n) % n
+                if next == cur or nums[next]*nums[cur] < 0: break
+                if next in mem: return True
+                mem[cur] = next
+                cur = next
+                
+        return False
         
+#nums = [2, 2, -1, 2]
+nums = [3, 1, 2]
+obj = Solution()
+print(obj.circularArrayLoop(nums))
--- code458PoorPigs.py
+++ code458PoorPigs.py
@@ -1,14 +1,18 @@
 """
 
-There are 1000 buckets, one and only one of them contains poison, the rest are filled with water. They all look the same. If a pig drinks that poison it will die within 15 minutes. What is the minimum amount of pigs you need to figure out which bucket contains the poison within one hour.
+There are 1000 buckets, one and only one of them contains poison, the rest are filled with water. 
+They all look the same. If a pig drinks that poison it will die within 15 minutes. 
+What is the minimum amount of pigs you need to figure out which bucket contains the poison within one hour.
 
 Answer this question, and write an algorithm for the follow-up general case.
 
 Follow-up:
 
-If there are n buckets and a pig drinking poison will die within m minutes, how many pigs (x) you need to figure out the "poison" bucket within p minutes? There is exact one bucket with poison.
+If there are n buckets and a pig drinking poison will die within m minutes, 
+how many pigs (x) you need to figure out the "poison" bucket within p minutes? There is exact one bucket with poison.
 
 """
+from math import ceil
 class Solution(object):
     def poorPigs(self, buckets, minutesToDie, minutesToTest):
         """
@@ -17,4 +21,4 @@ class Solution(object):
         :type minutesToTest: int
         :rtype: int
         """
-        
+        return ceil(buckets/ceil(minutesToTest/minutesToDie))
