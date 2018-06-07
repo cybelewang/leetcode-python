7737d38680184d6b31b7799a98bdc2960c9e65b2
Li_Guo <Li_Guo@PIDBLVWS0352.vti.pidnet.net>
2018-06-07 16:49:58 -0500

    464, 466

--- code464CanIWin.py
+++ code464CanIWin.py
@@ -26,10 +26,84 @@ The second player will win by choosing 10 and get a total = 11, which is >= desi
 Same with other integers chosen by the first player, the second player will always win.
 """
 class Solution:
+    # http://www.cnblogs.com/grandyang/p/6103525.html
+    # tricks better than my solution: 1. use remaining, 2. "used" can represent all scenarios, so "used" can be the key in cache, no need to use tuple(sum_, next choose number)
     def canIWin(self, maxChoosableInteger, desiredTotal):
         """
         :type maxChoosableInteger: int
         :type desiredTotal: int
         :rtype: bool
         """
-        
+        # precheck
+        if (maxChoosableInteger >= desiredTotal): return True
+        if desiredTotal > sum(range(1, maxChoosableInteger + 1)): return False
+
+        def dfs(remain, used, mem):
+            """
+            remain: remaining to achieve desiredTotal
+            used: each bit '1' represented that the position has been used
+            mem: cache with key = used, value = win or not win            
+            """
+            if used in mem:
+                return mem[used]
+
+            for i in range(0, maxChoosableInteger):
+                cur = 1 << i
+                if (cur & used) == 0:
+                    # i has not been used
+                    if remain <= i+1 or not dfs(remain-i-1, used | cur, mem):
+                        mem[used] = True
+                        return True
+                    
+            mem[used] = False
+            return False
+
+        return dfs(desiredTotal, 0, {})
+
+    # my own solution
+    def canIWin2(self, maxChoosableInteger, desiredTotal):
+        """
+        :type maxChoosableInteger: int
+        :type desiredTotal: int
+        :rtype: bool
+        """
+        # pre-check
+        if (maxChoosableInteger >= desiredTotal): return True
+        if desiredTotal > sum(range(1, maxChoosableInteger + 1)): return False
+
+        def dfs(sum_, used, mem):
+            """
+            sum_: previous sum
+            used: 32-bit integer with positions 1 to maxChoosableInteger's bits initially set to 1. For each bit, 0 represents it has been used, 1 represents it has not been used
+            mem: dict to cache the result, with key = (previous sum, currently selected number), value = win or not win for current player
+            """
+            # bug fixed: we should not use the below code to check win or not because this will forward win state to the other player
+            # if sum_ >= maxChoosableInteger:
+            #     return True
+            # elif used == 0:
+            #     return False
+
+            for i in range(1, maxChoosableInteger + 1):
+                if (1 << i) & used:
+                    # number i has not been used by any player
+                    # we should check if current player wins, with any possibility
+                    if sum_ + i >= desiredTotal:    
+                        return True
+
+                    if (sum_, i) in mem:
+                        win = mem[(sum_, i)]
+                    else:
+                        win = not dfs(sum_ + i, used^(1<<i), mem)   # current player's win state is the reverse of other play's win state
+                        mem[(sum_, i)] = win    # cache result
+                    
+                    if win:
+                        return True
+            
+            return False    # the current player will take any chance to win
+
+        default_used = ((1 << (maxChoosableInteger + 1)) - 1)^1
+
+        return dfs(0, default_used, {})
+
+obj = Solution()
+print(obj.canIWin(10, 20))
--- code466CountTheRepetitions.py
+++ code466CountTheRepetitions.py
@@ -1,9 +1,11 @@
 """
 Define S = [s,n] as the string S which consists of n connected strings s. For example, ["abc", 3] ="abcabcabc".
 
-On the other hand, we define that string s1 can be obtained from string s2 if we can remove some characters from s2 such that it becomes s1. For example, “abc” can be obtained from “abdbec” based on our definition, but it can not be obtained from “acbbe”.
+On the other hand, we define that string s1 can be obtained from string s2 if we can remove some characters from s2 such that it becomes s1. 
+For example, “abc” can be obtained from “abdbec” based on our definition, but it can not be obtained from “acbbe”.
 
-You are given two non-empty strings s1 and s2 (each at most 100 characters long) and two integers 0 ≤ n1 ≤ 106 and 1 ≤ n2 ≤ 106. Now consider the strings S1 and S2, where S1=[s1,n1] and S2=[s2,n2]. Find the maximum integer M such that [S2,M] can be obtained from S1.
+You are given two non-empty strings s1 and s2 (each at most 100 characters long) and two integers 0 ≤ n1 ≤ 10^6 and 1 ≤ n2 ≤ 10^6. 
+Now consider the strings S1 and S2, where S1=[s1,n1] and S2=[s2,n2]. Find the maximum integer M such that [S2,M] can be obtained from S1.
 
 Example:
 
@@ -14,7 +16,12 @@ s2="ab", n2=2
 Return:
 2
 """
+# similar to 418 sentence screen fitting
+from collections import defaultdict
 class Solution:
+    # http://bookshadow.com/weblog/2016/12/04/leetcode-count-the-repetitions/
+    # greedy algorithm: find the max length of s2 characters (may incomplete) while iterate all S1 characters
+    # trick is how to identify the start of repetition: use greedy to identify the long strings, but hash the short string indices
     def getMaxRepetitions(self, s1, n1, s2, n2):
         """
         :type s1: str
@@ -23,4 +30,52 @@ class Solution:
         :type n2: int
         :rtype: int
         """
-        
+        # pre-check letter set
+        if set(s2) > set(s1):
+            return 0
+
+        l1, l2 = len(s1), len(s2)   # unit length
+        dp = defaultdict(dict)
+        x1 = x2 = 0 # large indices
+        while x1 < l1 * n1:
+            # look for the next match
+            while s1[x1 % l1] != s2[x2 % l2]:
+                x1 += 1
+            if x1 >= l1 * n1:
+                break
+
+            # y1, y2 corresponding small indices of s1 and s2
+            y1, y2 = x1 % l1, x2 % l2
+
+            # find the first repetitive segment
+            if y2 not in dp[y1]:
+                dp[y1][y2] = x1, x2 # connect short indices with long indices
+            else:
+                # find the repeating segment
+                dx1, dx2 = dp[y1][y2]   # previous long indices
+                round = (l1 * n1 - dx1) // (x1 - dx1)   # skip the repetition, 
+                # goes to the last segment directly
+                x1 = dx1 + round * (x1 - dx1)
+                x2 = dx2 + round * (x2 - dx2)
+
+            if x1 < l1 * n1:
+                x1 += 1
+                x2 += 1
+        # now x2 is the length of s2 characters (including incomplete s2 characters)
+        return x2 // (n2 * l2)
+
+obj = Solution()
+print(obj.getMaxRepetitions("abacb", 6, "bcaa", 1))
+# also see http://www.cnblogs.com/grandyang/p/6149294.html
+
+"""
+s1 small index     01234   01234   01234   01234   01234   01234
+S1 large index     01234   56789   01234   56789   01234   56789
+S1 --------------> abacb | abacb | abacb | abacb | abacb | abacb
+S2 -------------->  b c    a a b      c    a a b      c    a a b      
+S2 large index      0 1    2 3 4      5    6 7 8      9    0 1 2
+s2 small index      0 1    2 3 0      1    2 3 0      1    2 3 0
+
+repeatCount ----->    0  |   1   |   1   |   2   |   2   |   3
+
+"""
