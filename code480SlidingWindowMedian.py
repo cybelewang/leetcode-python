f787f970512038f84148b46bfdaa4b6ab0ef85c9
Li_Guo <Li_Guo@PIDBLVWS0352.vti.pidnet.net>
2018-06-11 16:41:36 -0500

    480

--- code480SlidingWindowMedian.py
+++ code480SlidingWindowMedian.py
@@ -24,11 +24,30 @@ Therefore, return the median sliding window as [1,-1,-1,3,5,6].
 Note: 
 You may assume k is always valid, ie: k is always smaller than input array's size for non-empty array.
 """
+# similar problem 295
+from bisect import bisect_left, insort_left
 class Solution:
+    # reference http://zxi.mytechroad.com/blog/difficulty/hard/leetcode-480-sliding-window-median/
+    # also need to see other C++'s multiset solution
     def medianSlidingWindow(self, nums, k):
         """
         :type nums: List[int]
         :type k: int
         :rtype: List[float]
         """
-        
+        window = sorted(nums[:k])
+        res = []
+        for i in range(k, len(nums)+1):
+            res.append((window[k//2] + window[(k-1)//2])/2.0)   # trick to get the median: for odd k, k//2 and (k-1)//2 are the same, for even k, k//2 and (k-1)//2 are different
+            if i == len(nums):
+                break
+            index = bisect_left(window,nums[i-k])   # find the left item's index in the window O(logk)
+            window.pop(index)   # remove the left item O(k)
+            insort_left(window, nums[i])    # insert the right item into the window O(logk)
+
+        return res
+
+
+nums = [1,3,-1,-3,5,3,6,7]
+k = 3
+print(Solution().medianSlidingWindow(nums, k))    
