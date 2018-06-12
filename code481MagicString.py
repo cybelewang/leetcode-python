0073aa1f86999c964a2a64a24ac4435a087ffba1
Li_Guo <Li_Guo@PIDBLVWS0352.vti.pidnet.net>
2018-06-12 16:43:37 -0500

    481-482

--- code481MagicString.py
+++ code481MagicString.py
@@ -25,9 +25,32 @@ Output: 3
 Explanation: The first 6 elements of magical string S is "12211" and it contains three 1's, so return 3.
 """
 class Solution:
+    # my own solution, trick is to generate numbers from the 3rd number '2'
     def magicalString(self, n):
         """
         :type n: int
         :rtype: int
         """
-        
+        if n < 1: return 0
+        if n < 4: return 1
+
+        cur, magic = 1, [1, 2, 2]
+        i, j = 2, 3 # i is the iterator, j is the length of the magic string
+        res = 1
+        while j < n:
+            repeat = magic[i]
+            for _ in range(repeat):
+                magic.append(cur)
+            j += repeat
+            if cur == 1:
+                res += repeat
+
+            cur = 1 + ((cur-1)^1)   # switch between 1 and 2
+            i += 1
+
+        if j == n + 1 and magic[-1] == 1:
+            return res - 1
+        else:
+            return res
+
+print(Solution().magicalString(100000))
--- code482LicenseKeyFormatting.py
+++ code482LicenseKeyFormatting.py
@@ -1,7 +1,8 @@
 """
 You are given a license key represented as a string S which consists only alphanumeric character and dashes. The string is separated into N+1 groups by N dashes.
 
-Given a number K, we would want to reformat the strings such that each group contains exactly K characters, except for the first group which could be shorter than K, but still must contain at least one character. Furthermore, there must be a dash inserted between two groups and all lowercase letters should be converted to uppercase.
+Given a number K, we would want to reformat the strings such that each group contains exactly K characters, except for the first group which could be shorter than K, but still must contain at least one character. 
+Furthermore, there must be a dash inserted between two groups and all lowercase letters should be converted to uppercase.
 
 Given a non-empty string S and a number K, format the string according to the rules described above.
 
@@ -30,4 +31,9 @@ class Solution:
         :type K: int
         :rtype: str
         """
-        
+        S = S.replace('-','')[::-1]
+        return '-'.join(reversed([S[i:i+K][::-1].upper() for i in range(0, len(S), K)]))
+
+S = "2-5g-3--J"
+K = 2
+print(Solution().licenseKeyFormatting(S, K))
