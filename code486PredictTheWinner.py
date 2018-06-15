de6f817d52f925fba51b6d2a4b21fe596345e82a
Li_Guo <Li_Guo@PIDBLVWS0352.vti.pidnet.net>
2018-06-15 16:40:31 -0500

    486

--- code486PredictTheWinner.py
+++ code486PredictTheWinner.py
@@ -22,9 +22,96 @@ Any scores in the given array are non-negative integers and will not exceed 10,0
 If the scores of both players are equal, then player 1 is still the winner.
 """
 # similar problems: 375, 464
+# http://www.cnblogs.com/grandyang/p/6369688.html
 class Solution:
+    # DP
     def PredictTheWinner(self, nums):
         """
         :type nums: List[int]
         :rtype: bool
         """
+        n = len(nums)
+        if n < 3: return True
+        dp = [[-1]*n for _ in range(n)]
+        for i in range(n):
+            dp[i][i] = nums[i]
+        
+        for len_ in range(1, n):
+            i, j = 0, len_
+            while j < n:
+                dp[i][j] = max(nums[i]-dp[i+1][j], nums[j]-dp[i][j-1])
+                i += 1
+                j += 1
+        
+        return dp[0][n-1] >= 0
+
+    # recursive + DP
+    # the idea is if player 1 can win, then player1's score will >= half of the sum of nums
+    def PredictTheWinner2(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        def CanWin(nums, i, j, dp):
+            if dp[i][j] == -1:
+                dp[i][j] = nums[i] if i == j else max(nums[i] - CanWin(nums, i + 1, j, dp), nums[j] - CanWin(nums, i, j - 1, dp))
+            
+            return dp[i][j]
+        
+        n = len(nums)
+        if n < 3: return True
+        dp = [[-1]*n for _ in range(n)]
+
+        return CanWin(nums, 0, n-1, dp) >= 0
+
+    # recursive solution, easier to understand
+    def PredictTheWinner3(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        # this is the correct recursive function
+        def CanWin(nums, i, j, score1, score2, player):
+            if i == j:
+                if player == 1:
+                    return score1 + nums[i] >= score2
+                else:
+                    return score2 + nums[i] >= score1
+            else:
+                if player == 1:
+                    return not CanWin(nums, i+1, j, score1 + nums[i], score2, 2) or not CanWin(nums, i, j-1, score1 + nums[j], score2, 2)
+                else:
+                    return not CanWin(nums, i+1, j, score1, score2 + nums[i], 1) or not CanWin(nums, i, j-1, score1, score2 + nums[j], 1)
+
+        # this is a wrong recursive function?
+        def CanWin_Wrong(nums, i, j, score1, score2, player):
+            if i == j:  # just one number left
+                if player == 1:
+                    score1 += nums[i]
+                else:
+                    score2 += nums[i]
+
+                return score1 >= score2
+            elif j - i == 1:    # two numbers left
+                if player == 1:
+                    score1 += max(nums)
+                    score2 += min(nums)
+                else:
+                    score1 += min(nums)
+                    score2 += max(nums)
+
+                return score1 >= score2
+            else:   # three or more numbers, need recursive calls
+                if player == 1:
+                    return CanWin_Wrong(nums, i + 1, j, score1 + nums[i], score2, 2) or CanWin_Wrong(nums, i, j-1, score1 + nums[j], score2, 2)
+                else:
+                    return CanWin_Wrong(nums, i+1, j, score1, score2 + nums[i], 1) or CanWin_Wrong(nums, i, j-1, score1, score2 + nums[j], 1)
+
+        if len(nums) < 3:
+            return True
+        else:
+            return CanWin(nums, 0, len(nums)-1, 0, 0, 1)
+
+#nums = [1, 5, 7, 6]
+nums = [1, 5, 2]
+print(Solution().PredictTheWinner2(nums))
