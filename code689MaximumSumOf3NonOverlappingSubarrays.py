29e2564716e4fae33b9c330f29e4b9537acd6923
Li_Guo <Li_Guo@PIDBLVWS0352.vti.pidnet.net>
2018-08-22 16:51:48 -0500

    try 689

--- code689MaximumSumOf3NonOverlappingSubarrays.py
+++ code689MaximumSumOf3NonOverlappingSubarrays.py
@@ -16,6 +16,10 @@ nums[i] will be between 1 and 65535.
 k will be between 1 and floor(nums.length / 3).
 """
 class Solution:
+    # my own O(N^2) solution
+    # generate a list k_sum[i], value is the sum of k numbers starting from index i
+    # get a list max_k_sum[i], value is the index j in k_sum, with k_sum[j] is the largest in range k_sum[i:]
+    # then we iterate nums with i and j, and can find the third largest k_sum with the help of max_k_sum
     def maxSumOfThreeSubarrays(self, nums, k):
         """
         :type nums: List[int]
@@ -27,7 +31,7 @@ class Solution:
 
         k_sum[0] = sum(nums[:k])    # sum of k numbers starting from index i
         for i in range(1, n-k+1):
-            k_sum[i] = k_sum[i-1] - nums[i-1] + nums[i]
+            k_sum[i] = k_sum[i-1] - nums[i-1] + nums[i+k-1]
 
         max_k_sum_index = list(range(n-k+1))    # max_k_sum_index[i] is the index j, which has the max k sum for j in range [i:]
         for i in range(n-k-1, -1, -1):
@@ -35,8 +39,8 @@ class Solution:
                 max_k_sum_index[i] = max_k_sum_index[i+1]
 
         max_sum, res = 0, [0, k, 2*k]
-        for i in range(n-2*k):
-            for j in range(i+k, n-k):
+        for i in range(n-3*k+1):
+            for j in range(i+k, n-2*k+1):
                 current = k_sum[i] + k_sum[j] + k_sum[max_k_sum_index[j+k]]
                 if current > max_sum:
                     max_sum = current
@@ -46,6 +50,6 @@ class Solution:
         
         return res
 
-nums = [1,2,1,2,6,7,5,1]
+nums = list(range(20000))
 k = 2
 print(Solution().maxSumOfThreeSubarrays(nums, k))
