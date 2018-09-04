0f3c73a0322838b1551b304df8db9018adedfba8
Guo Li <guoligit@gmail.com>
2018-09-03 22:51:45 -0500

    Added 721-725

--- code718MaximumLengthOfRepeatedSubarray.py
+++ code718MaximumLengthOfRepeatedSubarray.py
@@ -12,8 +12,32 @@ Note:
 1 <= len(A), len(B) <= 1000
 0 <= A[i], B[i] < 100
 """
+# similar problems: 583, 712
 class Solution:
+    # my improved solution using O(n) space, note we must iterate from end to begin so when dp[j] = 1 + dp[j-1], dp[j-1] remains the last-i number
     def findLength(self, A, B):
+        """
+        :type A: List[int]
+        :type B: List[int]
+        :rtype: int
+        """
+        m, n = len(A), len(B)
+        dp = [0]*(n+1)
+
+        res = 0
+        for i in range(1, m+1):
+            for j in range(n, 0, -1):
+                if A[i-1] == B[j-1]:
+                    dp[j] = 1 + dp[j-1]
+                    res = max(res, dp[j])
+                else:
+                    dp[j] = 0
+        
+        return res
+
+    # my 1st trial, similar to find the max length of same subsequence using dynamic programming method
+    # here the difference is the problem requires continuous subarray
+    def findLength_Trial(self, A, B):
         """
         :type A: List[int]
         :type B: List[int]
