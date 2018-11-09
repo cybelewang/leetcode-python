77cb40fa1d465f32f9eec5b5651eaaff04878a31
Guo Li <guoligit@gmail.com>
2018-11-09 06:40:39 -0600

    861

--- code861ScoreAfterFlippingMatrix.py
+++ code861ScoreAfterFlippingMatrix.py
@@ -27,9 +27,34 @@ Note:
 A[i][j] is 0 or 1.
 """
 class Solution:
+    # my own solution
+    # for each row, make the first element 1
+    # then for each column, count 1 and 0, and add the most with weight   
     def matrixScore(self, A):
         """
         :type A: List[List[int]]
         :rtype: int
         """
         m, n = len(A), len(A[0])
+        # for each row, make the first element 1
+        for i in range(m):
+            if A[i][0] == 0:
+                for j in range(n):
+                    A[i][j] ^= 1
+
+        # now for each column, count 1 and 0, and add the most    
+        res, factor = 0, 2**(n-1)
+        for j in range(n):
+            count1 = 0
+            for i in range(m):
+                if A[i][j]:
+                    count1 += 1
+            
+            count1 = max(count1, m-count1)
+            res += count1*factor
+            factor //= 2
+        
+        return res
+
+A = [[0,0,1,1],[1,0,1,0],[1,1,0,0]]
+print(Solution().matrixScore(A))
