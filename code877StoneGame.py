281721bcb2dbbe3ab922af87c436319e53f24e2b
Li_Guo <Li_Guo@PIDBLVWS0352.vti.pidnet.net>
2018-11-20 16:41:32 -0600

    877

--- code877StoneGame.py
+++ code877StoneGame.py
@@ -19,7 +19,6 @@ Say he takes the first 5, so that the row becomes [3, 4, 5].
 If Lee takes 3, then the board is [4, 5], and Alex takes 5 to win with 10 points.
 If Lee takes the last 5, then the board is [3, 4], and Alex takes 4 to win with 9 points.
 This demonstrated that taking the first 5 was a winning move for Alex, so we return true.
- 
 
 Note:
 
@@ -28,10 +27,28 @@ piles.length is even.
 1 <= piles[i] <= 500
 sum(piles) is odd.
 """
+# similar problems: 464 Can I Win
 class Solution:
+    # help from https://leetcode.com/problems/stone-game/solution/
+    # change the game so that whenever Lee scores points, it deducts from Alex's score instead.
     def stoneGame(self, piles):
         """
         :type piles: List[int]
         :rtype: bool
         """
+        N = len(piles)
+
+        dp = [[0]*(N+2) for _ in range(N+2)]    # dp[i+1][j+1] = the value of the game [piles[i], ..., piles[j]].
+        for size in range(1, N+1):
+            for i in range(N-size+1):
+                j = i + size - 1
+                parity = (j + i + N) % 2    # 1 for Alex
+                if parity == 1:
+                    dp[i+1][j+1] = max(piles[i] + dp[i+2][j+1], piles[j] + dp[i+1][j])
+                else:
+                    dp[i+1][j+1] = min(-piles[i] + dp[i+2][j+1], -piles[j] + dp[i+1][j])
         
+        return dp[1][N] > 0
+
+piles = [5,3,4,5]
+print(Solution().stoneGame(piles))
