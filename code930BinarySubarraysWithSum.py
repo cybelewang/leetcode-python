06ec45f0f59f4c1f8f69fcd3f2366ff5b91c7559
Guo Li <guoligit@gmail.com>
2019-01-10 23:02:54 -0600

    Added 937-941

--- code930BinarySubarraysWithSum.py
+++ code930BinarySubarraysWithSum.py
@@ -57,6 +57,24 @@ class Solution:
 
         return ans
 
+    def numSubarraysWithSum_OJ(self, A, S):
+        indexes = [-1] + [ix for ix, v in enumerate(A) if v] + [len(A)]
+        ans = 0
+
+        if S == 0:
+            for i in xrange(len(indexes) - 1):
+                # w: number of zeros between two consecutive ones
+                w = indexes[i+1] - indexes[i] - 1
+                ans += w * (w+1) / 2
+            return ans
+
+        for i in xrange(1, len(indexes) - S):
+            j = i + S - 1
+            left = indexes[i] - indexes[i-1]
+            right = indexes[j+1] - indexes[j]
+            ans += left * right
+        return ans
+
 A, S = [1,0,1,0,1], 1
 #A, S = [0, 0, 0, 0], 0
 print(Solution().numSubarraysWithSum(A, S))
--- code931MinimumFallingPathSum.py
+++ code931MinimumFallingPathSum.py
@@ -28,7 +28,9 @@ class Solution:
         :type A: List[List[int]]
         :rtype: int
         """
-        m, n, ans = len(A), len(A[0]), 2**31 - 1
+        m, n, ans = len(A), len(A[0]), 2**31-1
+        if m == 1:  # bug fixed: corner case when len(A)==1
+            return min(A[0])
         for i in range(1, m):
             for j in range(n):
                 lo, hi = max(0, j-1), min(j+2, n)
@@ -37,7 +39,13 @@ class Solution:
                     ans = min(ans, A[i][j])
         
         return ans
+    def minFallingPathSum_OJ(self, A):
+        while len(A) >= 2:
+            row = A.pop()            
+            for i in xrange(len(row)):
+                A[-1][i] += min(row[max(0,i-1): min(len(row), i+2)])
+        return min(A[0])
 
-#A = [[1,2,3],[4,5,6],[7,8,9]]
-A = [[2, 1, -3], [-10, 10, 7], [-1, 10, -4]]
+A = [[1,2,3],[4,5,6],[7,8,9]]
+#A = [[2, 1, -3], [-10, 10, 7], [-1, 10, -4]]
 print(Solution().minFallingPathSum(A))
