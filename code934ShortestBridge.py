aa7fd7879b3eedbc2d7b7a1d06190e6a209002c8
Li_Guo <Li_Guo@PIDBLVWS0352.vti.pidnet.net>
2019-01-11 13:00:18 -0600

    934

--- code934ShortestBridge.py
+++ code934ShortestBridge.py
@@ -25,10 +25,64 @@ Note:
 1 <= A.length = A[0].length <= 100
 A[i][j] == 0 or A[i][j] == 1
 """
+from collections import deque
 class Solution:
+    # my own solution
+    # use BFS to mark the first encountered island to 2, then put all the board cells (contact 0) of the island to a queue
+    # next we will use another BFS to expand the island (marked as 2) outside, until it hits another island (marked as 1)
     def shortestBridge(self, A):
         """
         :type A: List[List[int]]
         :rtype: int
         """
-        
+        m, n = len(A), len(A[0])
+        dirs = [(0, -1), (-1, 0), (0, 1), (1, 0)]   # left, above, right, below
+        Q, front = deque(), deque()
+        for i in range(m):
+            for j in range(n):
+                if A[i][j] == 1:
+                    # see a cell for a island, use BFS to mark all connected cells to 2
+                    # in the BFS process, check if the cell connects to water (0), if so, it is a board cell, then we need to add it to front queue for further BFS
+                    Q.append((i, j))
+                    while Q:
+                        x, y = Q.popleft()
+                        isFront = False
+                        for dx, dy in dirs:
+                            p, q = x + dx, y + dy
+                            if -1 < p < m and -1 < q < n:
+                                if A[p][q] == 0:
+                                    isFront = True
+                                if A[p][q] == 1:
+                                    Q.append((p, q))
+                        if isFront:
+                            front.append((x, y))
+                        A[x][y] = 2 # mark this cell to 2
+                    break   # inner break
+            else:   # if no inner break, we continue to iterate to next i
+                continue
+            break   # outer break
+
+        # use another BFS to expand the front until touches another island
+        ans = 0
+        while front:
+            length = len(front)
+            for i in range(length):
+                i, j = front.popleft()                
+                for dx, dy in dirs:
+                    x, y = i + dx, j + dy
+                    if -1 < x < m and -1 < y < n:
+                        if A[x][y] == 1:    # front touches the other island
+                            return ans
+                        if A[x][y] == 0:
+                            front.append((x, y))
+                            A[x][y] = 2
+                
+            ans += 1
+
+        # dummy return
+        return ans
+
+#A = [[0,1],[1,0]]
+#A = [[0,1,0],[0,0,0],[0,0,1]]
+A = [[1,1,1,1,1],[1,0,0,0,1],[1,0,1,0,1],[1,0,0,0,1],[1,1,1,1,1]]
+print(Solution().shortestBridge(A))
