9307ff7e9e957693fd8faf1e636ece4a054f2ea2
Li_Guo <Li_Guo@PIDBLVWS0352.vti.pidnet.net>
2019-04-02 16:47:45 -0500

    990

--- code990SatisfiabilityOfEqualityEquations.py
+++ code990SatisfiabilityOfEqualityEquations.py
@@ -1,46 +1,32 @@
 """
-990. Satisfiability of Equality Equations
-Medium
+990 Satisfiability of Equality Equations
 
-140
-
-1
-
-Favorite
-
-Share
 Given an array equations of strings that represent relationships between variables, each string equations[i] has length 4 and takes one of two different forms: "a==b" or "a!=b".  Here, a and b are lowercase letters (not necessarily different) that represent one-letter variable names.
-
 Return true if and only if it is possible to assign integers to variable names so as to satisfy all the given equations.
 
- 
-
 Example 1:
-
 Input: ["a==b","b!=a"]
 Output: false
 Explanation: If we assign say, a = 1 and b = 1, then the first equation is satisfied, but not the second.  There is no way to assign the variables to satisfy both equations.
-Example 2:
 
+Example 2:
 Input: ["b==a","a==b"]
 Output: true
 Explanation: We could assign a = 1 and b = 1 to satisfy both equations.
-Example 3:
 
+Example 3:
 Input: ["a==b","b==c","a==c"]
 Output: true
-Example 4:
 
+Example 4:
 Input: ["a==b","b!=c","c==a"]
 Output: false
-Example 5:
 
+Example 5:
 Input: ["c==c","b==d","x!=z"]
 Output: true
- 
 
 Note:
-
 1 <= equations.length <= 500
 equations[i].length == 4
 equations[i][0] and equations[i][3] are lowercase letters
@@ -48,5 +34,42 @@ equations[i][1] is either '=' or '!'
 equations[i][2] is '='
 """
 class Solution:
-    def equationsPossible(self, equations: List[str]) -> bool:
+    # my union find solution
+    def equationsPossible(self, equations):
+        """
+        :type equations: list[str]
+        :rtype: bool
+        """
+        root = list(range(26))
+        def find(x):
+            """
+            union find
+            """
+            while root[x] != x:
+                x = root[x]            
+            return x
         
+        # parse equations
+        equal, not_equal = [], []
+        for e in equations:
+            a, b = ord(e[0]) - ord('a'), ord(e[3]) - ord('a')
+            if e[1] == '=':
+                equal.append((a, b))
+            else:
+                not_equal.append((a, b))
+        
+        # union those equals together
+        for a, b in equal:
+            ra, rb = find(a), find(b)
+            root[ra] = rb
+        
+        # now check those non-equals
+        for a, b in not_equal:
+            ra, rb = find(a), find(b)
+            if ra == rb:
+                return False
+        
+        return True
+
+equations = ["a==b","b!=c","c==a"]
+print(Solution().equationsPossible(equations))
