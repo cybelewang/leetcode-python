fb4ff8ac4ab5bb0f255194bd820de570137c3e7f
Guo Li <guoligit@gmail.com>
2019-04-22 23:35:12 -0500

    658

--- code658FindKClosestElements.py
+++ code658FindKClosestElements.py
@@ -49,6 +49,7 @@ class Solution:
 
     # 2nd visit on 4/22/2019
     # two pointers and greedy, O(n-k)
+    # wrong solution, can't pass test case [0,0,0,1,3,5,6,7,8,8], k=2, x=2
     def findClosestElements2(self, arr, k, x):
         i = 0   # start index of the result array
         for j in range(k, len(arr)):
@@ -59,7 +60,32 @@ class Solution:
         
         return arr[i:min(i+k, len(arr))]
 
-arr = [1,3,7,9,11]
-k = 4
-x = 6
-print(Solution().findClosestElements2(arr, k, x))
+    # time O(n-k), space O(1)
+    # solution 1 from https://www.cnblogs.com/grandyang/p/7519466.html
+    # greedy method, each time compare the front and back elements' distance to x, and remove the one with larger distance, until the remaining list size is k
+    def findClosestElements3(self, arr, k, x):
+        i, j = 0, len(arr)-1
+        while j - i + 1 > k:
+            if abs(arr[j]-x) < abs(arr[i]-x):
+                i += 1
+            else:
+                j -= 1
+        
+        return arr[i:i+k]
+
+    # best solution O(logN)
+    def findClosestElements4(self, arr, k, x):
+        left, right = 0, len(arr)-k # len(arr)-k is the most-right possible position, the final result should be <= right
+        while left < right:
+            mid = (left+right)//2
+            if x - arr[mid] > arr[mid+k] - x:
+                left = mid + 1
+            else:
+                right = mid
+        
+        return arr[right:right+k]
+
+arr = [0,0,0,1,3,5,6,7,8,8]
+k = 2
+x = 2
+print(Solution().findClosestElements4(arr, k, x))
