0551829ae22e1cfd18f3d305f951e3d42e2e2b78
Li_Guo <Li_Guo@PIDBLVWS0352.vti.pidnet.net>
2019-06-11 16:42:01 -0500

    123, 142

--- code123BestTimeToBuyAndSellStockIII.py
+++ code123BestTimeToBuyAndSellStockIII.py
@@ -45,7 +45,32 @@ class Solution:
         
         return res
 
+    # 2nd round solution on 6/11/2019
+    def maxProfit2(self, prices):
+        N = len(prices)
+        if N < 2:
+            return 0
+        
+        # left[i] is the max profit with up to ONE transaction on previous i days
+        # right[i] is the max profit with up to ONE transaction on last i days
+        left, right = [0]*(N+1), [0]*(N+1)
+        low = prices[0]
+        for i in range(1, N):
+            left[i+1] = max(left[i], prices[i] - low)
+            low = min(low, prices[i])
+
+        high = prices[N-1]
+        for i in range(N-1, -1, -1):
+            right[N-i] = max(right[N-i-1], high - prices[i])
+            high = max(high, prices[i])
+        
+        res = 0
+        for i in range(N+1):
+            res = max(res, left[i] + right[N-i])
+        
+        return res
+
 obj = Solution()
 test_cases = [[], [100], [7, 1, 5, 3, 6, 4], [7, 6, 4, 3, 1]]
 for case in test_cases:
-    print(obj.maxProfit(case))
+    print(obj.maxProfit2(case))
