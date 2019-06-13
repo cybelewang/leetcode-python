40312c95c4380a7753901c5fe56c92e252260087
guo <guo@guo-VirtualBox.com>
2019-06-13 13:12:55 -0500

    modified file properties

--- Interval.py
+++ Interval.py
@@ -8,3 +8,13 @@ class Interval(object):
     
     def __repr__(self): # __repr__ for printing this object in a iterable container
         return self.__str__()
+
+# test code
+if __name__ == "__main__":
+    A = [(1, 2), (3, 4)]
+    _list = []
+    for a in A:
+        _list.append(Interval(*a))
+    
+    print(_list)
+    
