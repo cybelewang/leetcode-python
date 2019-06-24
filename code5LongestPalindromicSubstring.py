10e9835c8344f102ad4871203a96f65fe9a25e3a
Li_Guo <Li_Guo@PIDBLVWS0352.vti.pidnet.net>
2019-06-24 16:44:43 -0500

    5, 20

--- code5LongestPalindromicSubstring.py
+++ code5LongestPalindromicSubstring.py
@@ -34,4 +34,31 @@ class Solution(object):
         
         return s[i: j+1]
 
-# 2nd visit on 9/14/2018                
+    # Manacher Algorithm, https://www.cnblogs.com/grandyang/p/4475985.html
+    # 6/24/2019
+    def longestPalindrome2(self, s):
+        # insert '$' and '#' to change from "abc" to "$#a#b#c"
+        t = '$#' + '#'.join(s) + '#'
+        N = len(t)
+
+        # process t
+        p = [0]*N
+        mx, id, resLen, resCenter = 0, 0, 0, 0
+        for i in range(1, N):
+            p[i] = min(p[2*id - i], mx - i) if mx > i else 1
+            while t[i+p[i]] == t[i-p[i]]:
+                p[i] += 1
+            
+            if mx < i + p[i]:
+                mx = i + p[i]
+                id = i
+            
+            if resLen < p[i]:
+                resLen = p[i]
+                resCenter = i
+        
+        start = (resCenter - resLen)//2
+        return s[start:start+resLen-1]
+
+s = "122122"
+print(Solution().longestPalindrome2(s))
